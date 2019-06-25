cec8a87c9ecbf9915ef7e7f50cc2716e733caab3
Nathan Fulton <nathan@ibm.com>
2019-06-25 10:13:22 -0400

    Remove R stubs and put in vectors.
    Couldn't find a better solution than Coq.Vectors.Vector...

--- NN.v
+++ NN.v
@@ -2,6 +2,7 @@ Require Import Reals.Rbase.
 Require Import Reals.Rfunctions.
 Require Import Arith.
 Require Import String.
+Require Import Vector.
 
 Module NN.
 
@@ -37,6 +38,18 @@ Fixpoint DF_apply (e: DefinedFunction) (args: string -> DefinedFunction) :=
 
 End DefinedFunctions.
 
+Section Vectors.
+(* I'm using Coq.Vectors.Vector. *)
+
+(* The type of real-valued vectors *)
+Definition rvector : nat -> Set := t R.
+
+(* A norm on real-valued vectors
+ * TODO define a proper norm *)
+Fixpoint norm (n: nat) (vector: rvector n) := R0.
+
+End Vectors.
+
 Section SeriesDivergence.
 
 Definition converges (s: nat -> R) :=
@@ -71,15 +84,15 @@ Definition Assumption_C_1 (ak : nat -> R) : Prop :=
     converges ak_squared.
 
 (* note: classical sup = LEM, and isn't needed anyway as long as we can specialize the definition of the stochastic subgradients a little bit to be a sequence indexable by nat *)
-Definition Assumption_C_2 (x: nat -> R) : Prop :=
+Definition Assumption_C_2 (d: nat) (x: nat -> rvector d) : Prop :=
   exists M : R,
-    forall k: nat, Rabs (x k) < M.
+    forall k: nat, (norm d (x k)) < M.
 
 (* TODO *)
 Definition Assumption_C_3 := 1=1.
 
-Definition Assumption_C (alpha: nat -> R) (x : nat -> R) : Prop :=
-  Assumption_C_1 alpha /\ Assumption_C_2 x /\ Assumption_C_3.
+Definition Assumption_C (d: nat) (alpha: nat -> R) (x : nat -> rvector d) : Prop :=
+  Assumption_C_1 alpha /\ Assumption_C_2 d x /\ Assumption_C_3.
 
 Local Close Scope R_scope.
 
