fe64acd923a8821e2bbf7a1d938e2acf2bf6d41a
Avi Shinnar <shinnar@us.ibm.com>
2019-07-16 11:54:58 -0400

    Simplify the proof of telescope

--- coq/Lebesgue.v
+++ coq/Lebesgue.v
@@ -33,56 +33,37 @@ Proof.
   simpl.
   unfold Partition.
   repeat rewrite removelast_map, tl_map.
-  repeat rewrite fold_right_map.
   rewrite removelast_seq, tl_seq.
   replace ((n + 1 - 1)%nat) with n by omega.
 
   (* B *)
-  destruct n; [ omega | ].
+  destruct n ; [ omega | ].
   replace (seq 0 (S n)) with (0%nat :: seq 1 n) by reflexivity.
   rewrite seq_Sn.
-
-  assert (eqq:forall (l:list nat),
-             fold_right (fun a0 b0 => f (a + (INR a0) * ((b - a) / INR (S n))) + b0) 0 l =
-             fold_right Rplus 0
-                        (map f (map (fun i : nat => a + INR i * ((b - a) / INR (S n))) l))).
-  - intros.
-    repeat rewrite fold_right_map; trivial.
-  - repeat rewrite eqq.
-    
-    replace (fold_right Rplus 0 (map f
-         (map (fun i : nat => a + INR i * ((b - a) / INR (S n))) (seq 1 n ++ (1 + n)%nat :: Datatypes.nil))))
-                                     with 
-    (fold_right Rplus 0 (map f
-         (map (fun i : nat => a + INR i * ((b - a) / INR (S n))) ((1 + n)%nat :: seq 1 n)))).
-    + (* C *)
-      repeat rewrite map_cons.
-      simpl fold_right.
-      generalize (fold_right Rplus 0
-    (map f
-       (map (fun i : nat => a + INR i * ((b - a) / match n with
-                                                   | 0%nat => 1
-                                                   | S _ => INR n + 1
-                                                   end)) (seq 1 n)))); intros ?.
-      rewrite Rmult_0_l.
-      replace (match n with
-      | 0%nat => 1
-      | S _ => INR n + 1
-               end) with (INR (S n)) by reflexivity.
-      field_simplify.
-      repeat f_equal.
-      * lra.
-      * rewrite (Rmult_comm (INR (S n))).
-        unfold Rdiv.
-        rewrite Rmult_assoc.
-        rewrite <- Rinv_l_sym; [lra | ].
-        generalize (pos_INR n); intros.
-        rewrite S_INR.
-        lra.
-    + apply fold_right_perm.
-      * intros; lra.
-      * intros; lra.
-      * apply Permutation_map.
-        rewrite Permutation_app_comm.
-        trivial.
+  match goal with
+  | [|- context [fold_right ?f ?init (map ?f1 (map ?f2 (seq 1 n ++ ((1 + n)%nat::nil))))] ] =>
+    replace (fold_right f init (map f1 (map f2 (seq 1 n ++ ((1 + n)%nat::nil)))))
+      with (fold_right f init (map f1 (map f2 ((1 + n)%nat :: seq 1 n))))
+  end.
+  + (* C *)
+    repeat rewrite map_cons.
+    Opaque INR.
+    simpl.
+    field_simplify.
+    Transparent INR.
+    repeat f_equal.
+    * simpl; lra.
+    * rewrite (Rmult_comm (INR (S n))).
+      unfold Rdiv.
+      rewrite Rmult_assoc.
+      rewrite <- Rinv_l_sym; [lra | ].
+      generalize (pos_INR n); intros.
+      rewrite S_INR.
+      lra.
+  + apply fold_right_perm.
+    * intros; lra.
+    * intros; lra.
+    * apply Permutation_map.
+      rewrite Permutation_app_comm.
+      trivial.
 Qed.
