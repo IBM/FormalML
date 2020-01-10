57624a16d3bf847b06fca396b2ef87f8192a33ce
Avi Shinnar <shinnar@us.ibm.com>
2020-01-10 14:05:12 -0500

    Change extraction test example to use a negative

--- ocaml/NNOptExtraction.v
+++ ocaml/NNOptExtraction.v
@@ -15,14 +15,13 @@ Require Gen_NN.
 Require Import DefinedFunctions.
 Require Import FloatishDef.
 Require Import String.
-Local Open Scope string.
 Local Open Scope list.
 
 Existing Instance floatish_IEEE.
 
 Example test :=
   mk_env_entry (Name "f", DTfloat) (FfromZ 1) ::
-               mk_env_entry (Name "v", DTVector 3) (ConstVector 3 (FfromZ 2)) :: 
+               mk_env_entry (Name "v", DTVector 3) (ConstVector 3 (Fopp (FfromZ 2))) :: 
                mk_env_entry (Name "m", DTMatrix 2 3) (ConstMatrix 2 3 (FfromZ 3)) :: nil.
 Module API.
   Example opt := @Gen_NN.opt floatish_IEEE.
