15a6b06c79518b6ed5e323f3794748b9fc0c8a95
Avi Shinnar <shinnar@us.ibm.com>
2020-01-10 14:14:03 -0500

    fix problem in test for extraction

--- ocaml/NNOptExtraction.v
+++ ocaml/NNOptExtraction.v
@@ -14,15 +14,16 @@ Require Import ExtrFloatishIEEE.
 Require Gen_NN.
 Require Import DefinedFunctions.
 Require Import FloatishDef.
+Require Import BinInt.
 Require Import String.
 Local Open Scope list.
 
 Existing Instance floatish_IEEE.
 
 Example test :=
-  mk_env_entry (Name "f", DTfloat) (FfromZ 1) ::
-               mk_env_entry (Name "v", DTVector 3) (ConstVector 3 (Fopp (FfromZ 2))) :: 
-               mk_env_entry (Name "m", DTMatrix 2 3) (ConstMatrix 2 3 (FfromZ 3)) :: nil.
+  mk_env_entry (Name "f", DTfloat) (FfromZ 1)%Z ::
+               mk_env_entry (Name "v", DTVector 3) (ConstVector 3 ((FfromZ (-2)))%Z) :: 
+               mk_env_entry (Name "m", DTMatrix 2 3) (ConstMatrix 2 3 (FfromZ 3))%Z :: nil.
 Module API.
   Example opt := @Gen_NN.opt floatish_IEEE.
   Example test_env := test.
