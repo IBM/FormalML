b58e3ea9462388b9487fb41e7007a78e6d0fa39d
Avi Shinnar <shinnar@us.ibm.com>
2020-01-14 11:34:03 -0500

    add example Coq function to remove the first column and call it

--- ocaml/nnopt.ml
+++ ocaml/nnopt.ml
@@ -9,4 +9,5 @@ let () = Format.printf "Result of running opt: %a\n" (pretty_visible_option pret
 let () = Format.printf "The test environment: %a\n" pretty_df_env test_env ;;
 
 let data = read_int_matrix_from_csv "breast-cancer-wisconsin.data" ;;
-let () = Format.printf "first part of data: %d" (List.hd (List.hd data))
+let actual_data = discard_first data ;;
+let () = Format.printf "first part of data without the first column: %d" (List.hd (List.hd actual_data))
