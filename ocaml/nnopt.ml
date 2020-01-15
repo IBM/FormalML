e07d73394b0934d48f3efb37fa0f85ffe8c3c562
Avi Shinnar <shinnar@us.ibm.com>
2020-01-15 09:28:45 -0500

    update example for random matrix

--- ocaml/nnopt.ml
+++ ocaml/nnopt.ml
@@ -10,9 +10,9 @@ let () = Format.printf "The test environment: %a\n" pretty_df_env test_env ;;
 
 let data = read_int_matrix_from_csv "breast-cancer-wisconsin.data" ;;
 let actual_data = discard_first data ;;
-let () = Format.printf "first part of data without the first column: %d" (List.hd (List.hd actual_data))
+let () = Format.printf "first part of data without the first column: %d\n" (List.hd (List.hd actual_data))
 
 let () = Random.self_init()
 let rm = random_float_matrix ();;
-let () = Format.printf "random matrix: %a" (pretty_matrix 8 4) (Obj.magic rm);;
-    
+let () = Format.printf "random matrix smaller: %a\n" (pretty_matrix 3 4) (Obj.magic rm);;    
+let () = Format.printf "random matrix bigger: %a\n" (pretty_matrix 8 4) (Obj.magic rm);;
