fd76183c0170ff46158da997e1c64314ba1d8789
Avi Shinnar <shinnar@us.ibm.com>
2020-01-15 09:26:59 -0500

    Add ocaml utility code for creating a random vector or matrix of floats between 0 and 1, and a top level example using it

--- ocaml/src/Util.mli
+++ ocaml/src/Util.mli
@@ -2,3 +2,8 @@ val string_of_char_list : char list -> string
 val char_list_of_string : string -> char list
 
 val read_int_matrix_from_csv : string -> int list list
+
+val memoized_vector : (int -> 'a) -> int -> 'a
+
+val random_float_vector : unit -> int -> float
+val random_float_matrix : unit -> int -> int -> float
