2fbd32e53b87a97ce8d200aa69b5b813c67a7cfd
Barry M. Trager <bmt@us.ibm.com>
2020-01-30 13:03:04 -0500

    bug fixing

--- ocaml/nnopt.ml
+++ ocaml/nnopt.ml
@@ -6,6 +6,15 @@ open Pretty
 
 
 let () = Format.printf "Result of running opt: %a\n" (pretty_visible_option pretty_df_env) opt ;;
+let () = Format.printf "Result of running opt2: %a\n" (pretty_visible_option pretty_df_env) opt2 ;;
+let () = Format.printf "The testopt environment: %a\n" pretty_df_env testopt ;;
+let () = Format.printf "The testreeopt environment: %a\n" pretty_df_env testreeopt ;;
+
+let () = Format.printf "The gradenv environment: %a\n" pretty_df_env gradenv ;;
+let () = Format.printf "The gradenv_tree environment: %a\n" pretty_df_env gradenv_tree ;;
+
+let () = Format.printf "The test_update environment: %a\n" pretty_df_env test_update ;;
+
 let () = Format.printf "The test environment: %a\n" pretty_df_env test_env ;;
 
 let data = read_int_matrix_from_csv "breast-cancer-wisconsin.data" ;;
@@ -26,7 +35,10 @@ let init_env = init_env2 9 15 1 (char_list_of_string "w") (char_list_of_string "
 let () = Format.printf "Init environment: %a\n" pretty_df_env init_env ;;
 
 let wval = eval_wisconsin_batch 500 (Obj.magic init_env) (Obj.magic normalized_data) ;;
+let () = Format.printf "wisconsin lost value : %a\n" (pretty_blist pp_print_float) (Obj.magic wval) ;;
 
+let wval2 = wisconsin_test 500 10 (Obj.magic init_env) (Obj.magic normalized_data) ;;
+let () = Format.printf "wisconsin lost value : %a\n" (pretty_blist pp_print_float) (Obj.magic wval) ;;
 
 
 
