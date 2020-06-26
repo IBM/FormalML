3e6837e5f11a1ef63f6a28c810b4f3505b855569
Koundinya Vajjha <Koundinya.Vajjha@ibm.com>
2020-06-26 14:45:20 -0400

    Add bind and return for pmf_monad anew.

--- coq/converge/pmf_monad.v
+++ coq/converge/pmf_monad.v
@@ -1,18 +1,52 @@
 Require Import Reals Coquelicot.Coquelicot Sums.
-Require Import Reals Fourier FunctionalExtensionality Psatz.
-Require Import Coq.Logic.ProofIrrelevance Coq.Bool.Bool.
-Require Import Setoid Program.
-Require Import Paco.paco.
-Require Import Reals Fourier FunctionalExtensionality Psatz.
+Require Import Fourier FunctionalExtensionality Psatz ProofIrrelevance.
 From mathcomp Require Import ssreflect ssrbool ssrfun eqtype ssrnat seq div choice fintype finset finfun bigop.
 
-(* Joe's ldist. Renamed to Pmf. *)
+
+Set Bullet Behavior "Strict Subproofs".
 
 
-Record Pmf (A: Type) := mkPmf {
-  outcomes :> list (R * A);
-  nonneg : all  (fun r => Rle_dec 0 r) [seq r.1 | r <- outcomes];
-  sum1 : \big[Rplus/0]_(a <- map fst outcomes) a = R1
+(*
+
+****************************************************************************************
+This file defines the pmf monad (also called the finitary Giry monad) which is a monad 
+of finitely supported probability measures on a set. The construction is very general, 
+and we don't need to work in that generality. We instead work with a finite state space, 
+since that is what we will use in our construction of MDPs. 
+****************************************************************************************
+
+ *)
+
+Check finset.
+Check powerset.
+Check cond_pos.
+
+Fixpoint list_fst_sum {A : Type} (l : list (nonnegreal*A)): R  :=
+  match l with
+  | nil => 0
+  | (n,_) :: ns => n + list_fst_sum ns                
+  end.
+
+Lemma list_sum_is_nonneg {A : Type} (l : list(nonnegreal*A)) : 0 <= list_fst_sum l. 
+Proof.
+  induction l.
+  simpl ; lra.
+  simpl. destruct a as [n].
+  assert (0 <= n) by apply (cond_nonneg n).
+  apply (Rplus_le_le_0_compat _ _ H).
+  lra.
+Qed.
+
+
+Definition nonneg_list_sum {A : Type} (l : list (nonnegreal * A)) : nonnegreal
+  := {|
+  nonneg := list_fst_sum l;
+  cond_nonneg := (list_sum_is_nonneg l)
+|}.
+                                                                         
+Record Pmf (A : Type) := mkPmf {
+  outcomes :> list (nonnegreal * A);
+  sum1 : list_fst_sum outcomes = R1
  }.
 
  Arguments outcomes {_}.
@@ -20,53 +54,29 @@ Record Pmf (A: Type) := mkPmf {
  Arguments mkPmf {_}.
  
 
-Lemma Pmf_ext  (A : Type) (p q : Pmf A)  : outcomes p = outcomes q -> p = q.
+Lemma Pmf_ext  {A} (p q : Pmf A)  : outcomes p = outcomes q -> p = q.
 Proof.
-destruct p as [op  np sp].
-destruct q as [oq  nq sq]. rewrite /outcomes => ?.
-subst. f_equal. apply bool_irrelevance. apply proof_irrelevance.
-Qed.
-
-(*Definition Pmf_support A (p : Pmf A) : list A := filter (fun x => _) _. *)
-
-Lemma pure_nonneg {A} (a : A) : is_true (all (fun r : R => Rle_dec 0 r) [seq r.1 | r <- [:: (R1, a)]]).
-Proof.
-simpl.
-rewrite andb_true_r.
-destruct Rle_dec => //=. nra.
-Qed.
-
-Lemma pure_sum1 {A} (a : A) : \big[Rplus/0]_(a <- [seq i.1 | i <- [:: (R1, a)]]) a = R1. 
-Proof.
-  simpl. rewrite big_cons. rewrite big_nil. nra.
+destruct p as [op sp].
+destruct q as [oq sq].
+rewrite /outcomes => ?. (* what's happening here? *)
+subst. f_equal. apply proof_irrelevance.
 Qed.
 
 
-Definition Pmf_pure A (a : A) : Pmf A := {|
-outcomes := [:: (R1,a)];
-nonneg :=  pure_nonneg a;
-sum1 := pure_sum1 a
+Lemma pure_sum1 {A} (a : A) : list_fst_sum [:: ({| nonneg := R1; cond_nonneg := Rlt_le 0 1 Rlt_0_1 |}, a)] = R1. 
+Proof.
+  simpl. nra.
+Qed.
+
+Definition Pmf_pure {A} (a : A) : Pmf A := {|
+outcomes := [::(mknonnegreal R1 (Rlt_le _ _ Rlt_0_1),a)];
+sum1 := pure_sum1 _
 |}.
 
-Definition dist_bind_aux :=
-  fun {A B} (f: A -> Pmf B) =>
-  fix go (l: seq (R * A)) :=
-    match l with
-      [::] => [::]
-    | (r, x) :: l => map (fun py => (r * py.1, py.2)%R) (f x) ++ go l
-    end.
 
-(*Check dist_bind_aux.
-Check allP.
-
-Lemma dist_bind_pf1 {A B} (f: A -> Pmf B) d:
-  all [eta (Rle_dec 0)] [seq r.1 | r <- dist_bind_aux f (outcomes d)].
-Proof.
-  Admitted.
-
-  
-Definition Pmf_bind {A B} (p : Pmf A) (f : A -> Pmf B) : Pmf B := {|
-  outcomes := dist_bind_aux f p.(outcomes);
-  nonneg := dist_bind_pf1 f p;
-  sum1 := _
-  |}.*)
+Fixpoint dist_bind_outcomes
+         {A B : Type} (f : A -> list(nonnegreal*B)) (p : list (nonnegreal*A)) : list(R*B) :=
+  match p with
+   | nil => nil
+   | (n,a) :: ps => map (fun (py:nonnegreal*B) => (n*py.1,py.2)) (f a) ++ (dist_bind_outcomes f ps)
+  end.
