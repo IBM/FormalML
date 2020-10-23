3873d08bc829479b7ac275440997c4d6536115fd
Barry M. Trager <bmt@us.ibm.com>
2020-10-23 12:21:49 -0400

    cleanup

--- coq/BorelSigmaAlgebra.v
+++ coq/BorelSigmaAlgebra.v
@@ -9,11 +9,21 @@ Instance open_borel_sa : SigmaAlgebra R
 Instance borel_sa : SigmaAlgebra R
   := generated_sa (fun (s:R->Prop) => exists r, forall m, m <= r <-> s m)%R.
 
-Lemma borel_sa_preimage 
+Lemma borel_sa_preimage
       {Ts:Type}
       {dom: SigmaAlgebra Ts}
       (rvx: Ts -> R)
-      (pf:forall r:R, sa_sigma (fun omega:Ts => (rvx omega) <= r)%R) : 
-  forall B: event R, (sa_sigma (event_preimage rvx B)).
+      (pf_pre: forall r:R, sa_sigma (fun omega:Ts => (rvx omega) <= r)%R) :
+  (forall B: event R, sa_sigma B -> sa_sigma (event_preimage rvx B)).
+Proof.
+Admitted.
+
+
+Lemma borel_sa_preimage2 
+      {Ts:Type}
+      {dom: SigmaAlgebra Ts}
+      (rvx: Ts -> R):
+  (forall r:R, sa_sigma (fun omega:Ts => (rvx omega) <= r)%R) <-> 
+  (forall B: event R, sa_sigma B -> (sa_sigma (event_preimage rvx B))).
 Proof.
 Admitted.
