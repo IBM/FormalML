ba075104017f36cec81c2fb4e96468d9c084a6f7
Avi Shinnar <shinnar@us.ibm.com>
2020-11-12 09:29:01 -0500

    work on Dvoretzky_rel0

--- coq/Dvoretzky.v
+++ coq/Dvoretzky.v
@@ -17,20 +17,32 @@ Set Bullet Behavior "Strict Subproofs".
    {prts: ProbSpace dom}.
 
  (* scalar version of T *)
- 
+
+ Global Instance rvsqr_proper {Ts} : Proper (rv_eq ==> rv_eq) (@rvsqr Ts).
+ Proof.
+   unfold rv_eq, rvsqr, Proper, respectful, pointwise_relation.
+   intros x y eqq z.
+   unfold Rsqr.
+   rewrite eqq.
+   trivial.
+ Qed.
+   
+
 Lemma Dvoretzky_rel0 (n:nat) (T X Y : nat -> R -> R) (F : nat -> R)
       (svy : SimpleRandomVariable (Y n)) 
       (svx: SimpleRandomVariable (X n))
       (svx2: SimpleRandomVariable (X (S n))) :
   (forall (n:nat) (r:R), Rle (Rabs (T n r)) (F n * Rabs r)) ->
-  (forall (n:nat), X (S n) = rvplus (fun r =>  T n (X n r)) (Y n)) ->
+  (forall (n:nat), rv_eq (X (S n)) (rvplus (fun r =>  T n (X n r)) (Y n))) ->
   Rle (SimpleExpectation (rvsqr (X (S n)) ))
       ((Rsqr (F n)) * SimpleExpectation (rvsqr (X n))
        + SimpleExpectation (rvsqr (Y n))).
  Proof.
    intros.
    specialize (H0 n).
-   rewrite H0.
+   rewrite (SimpleExpectation_transport (srvsqr (X (S n))) (rvsqr_proper _ _ H0)).
+
+Admitted.
    
                          
 Lemma Dvoretzky_rel (n:nat) (theta:R) (T X Y : nat -> R -> R) (F : nat -> R)
