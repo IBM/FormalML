59ad3260a8cf1411a3106e4adbeafbb3a217c800
Avi Shinnar <shinnar@us.ibm.com>
2020-11-12 14:19:18 -0500

    WIP

--- coq/Dvoretzky.v
+++ coq/Dvoretzky.v
@@ -26,6 +26,45 @@ Set Bullet Behavior "Strict Subproofs".
    rewrite eqq.
    trivial.
  Qed.
+
+ Global Instance R_nonempty : NonEmpty R
+   := R0.
+
+ Global Instance rvmult_proper {Ts} : Proper (rv_eq ==> rv_eq ==> rv_eq) (@rvmult Ts).
+ Proof.
+   unfold rv_eq, rvmult.
+   intros ???????.
+   now rewrite H, H0.
+ Qed.
+
+
+ Lemma SimpleExpectation_const c srv : SimpleExpectation (const c) (srv:=srv) = c.
+ Proof.
+   rewrite (SimpleExpectation_pf_irrel _ (srvconst c)).
+   unfold SimpleExpectation; simpl.
+   unfold RandomVariable.srvconst_obligation_1.
+   unfold event_preimage, singleton_event, const.
+   erewrite ps_proper.
+     - erewrite ps_one.
+       lra.
+     - unfold Ω.
+       red; intros; intuition.
+ Qed.
+
+ Existing Instance rvscale_rv.
+ Existing Instance rvplus_rv.
+Existing Instance rvmult_rv.
+
+ Declare Scope rv.
+
+ Infix ".+" := rvplus (left associativity, at level 50) : rv.
+ Infix ".-" := rvminus (left associativity, at level 50) : rv.
+ Infix ".*" := rvmult (left associativity, at level 40) : rv.
+ Infix ".*." := rvscale (no associativity, at level 40) : rv.
+ Notation "x .²" := (rvsqr x) (at level 1) : rv.
+
+ Local Open Scope rv.
+
    
 Lemma Dvoretzky_rel00 (n:nat) (T X Y : nat -> R -> R) (F : nat -> R) {nempty:NonEmpty R}
       (rvy : RandomVariable prts borel_sa (Y n)) 
@@ -47,67 +86,45 @@ Lemma Dvoretzky_rel00 (n:nat) (T X Y : nat -> R -> R) (F : nat -> R) {nempty:Non
        + SimpleExpectation (rvsqr (Y n))).
  Proof.
    intros.
-   rewrite <- sumSimpleExpectation.
-   rewrite <- sumSimpleExpectation.
+   rewrite <- sumSimpleExpectation; try typeclasses eauto.
+   rewrite <- sumSimpleExpectation; try typeclasses eauto.
    rewrite <- scaleSimpleExpectation.
    rewrite <- Rplus_assoc.
    apply Rplus_le_compat_r.
    generalize (conditional_tower_law (rvmult (fun r : R => T n (X n r)) (Y n)) (X n)) ; intros.
    generalize (conditional_scale_measurable (fun r:R => T n (X n r)) (Y n) (X n)); intros.
    cut_to H3.
-   specialize (H2 (rvmult_rv (fun r : R => T n (X n r)) (Y n)) rvx).
-   specialize (H2 (srvmult (fun r : R => T n (X n r)) (Y n)) svx).
-   rewrite <- H2.
+   - specialize (H2 (rvmult_rv (fun r : R => T n (X n r)) (Y n)) rvx).
+     specialize (H2 (srvmult (fun r : R => T n (X n r)) (Y n)) svx).
+     rewrite <- H2.
+     rewrite (SimpleExpectation_transport _ H3).
+     assert (eqq4:rv_eq  (rvmult (fun r : R => T n (X n r)) (SimpleConditionalExpectation (Y n) (X n)))
+                         (const 0)).
+     {
+       rewrite H1.
+       unfold rvmult, const; intros ?; simpl; field.
+     } 
+     rewrite (SimpleExpectation_transport _ eqq4).
+     rewrite SimpleExpectation_const.
+     rewrite Rmult_0_r .
+     rewrite Rplus_0_r .
+     specialize (H n).
+     rewrite (scaleSimpleExpectation (Rsqr (F n))).
 
-(*
-   rewrite H3.
- *)
+     apply SimpleExpectation_le; try typeclasses eauto.
+     unfold RealRandomVariable_le.
+     intros.
+     unfold rvsqr, rvscale.
+     specialize (H (X n x)).
+     rewrite <- Rabs_right with (r:=F n) in H; trivial.
+     rewrite <- Rabs_mult in H.
+     apply Rsqr_le_abs_1 in H.
+     rewrite Rsqr_mult in H.
+     apply H.
+   - admit.
+ Admitted.
 
-   assert 
-       (@SimpleExpectation R dom prts
-             (@SimpleConditionalExpectation R dom prts
-                (@rvmult R (fun r : R => T n (X n r)) (Y n)) (X n)
-                (@srvmult R (fun r : R => T n (X n r)) (Y n) svt svy) svx)
-             (@SimpleConditionalExpectation_simple R dom prts
-                (@rvmult R (fun r : R => T n (X n r)) (Y n)) (X n)
-                (@rvmult_rv R dom prts (fun r : R => T n (X n r)) (Y n) rvt rvy) rvx
-                (@srvmult R (fun r : R => T n (X n r)) (Y n) svt svy) svx) = 0).
-   admit.
-   rewrite H4.
-   rewrite Rmult_0_r .
-   rewrite Rplus_0_r .
-   specialize (H n).
-   rewrite (scaleSimpleExpectation (Rsqr (F n))).
-   apply SimpleExpectation_le.
-   now apply rvsqr_rv.
-   apply rvscale_rv.
-   now apply rvsqr_rv.
-   unfold RealRandomVariable_le.
-   intros.
-   unfold rvsqr, rvscale.
-   specialize (H (X n x)).
-   rewrite <- Rabs_right with (r:=F n) in H; trivial.
-   rewrite <- Rabs_mult in H.
-   apply Rsqr_le_abs_1 in H.
-   rewrite Rsqr_mult in H.
-   apply H.
-   admit.
-   apply rvscale_rv.
-   now apply rvmult_rv.
-   now apply rvsqr_rv.
-   trivial.
-   now apply rvsqr_rv.
-   apply rvplus_rv.
-   apply rvscale_rv.
-   now apply rvmult_rv.
-   now apply rvsqr_rv.
-   trivial.
-   
-
-   Admitted.
-   
-
-
+ 
 Lemma Dvoretzky_rel0 (n:nat) (T X Y : nat -> R -> R) (F : nat -> R)
       (svy : SimpleRandomVariable (Y n)) 
       (svx: SimpleRandomVariable (X n))
@@ -122,22 +139,18 @@ Lemma Dvoretzky_rel0 (n:nat) (T X Y : nat -> R -> R) (F : nat -> R)
    intros.
    specialize (H0 n).
    rewrite (SimpleExpectation_transport (srvsqr (X (S n))) (rvsqr_proper _ _ H0)).
-   assert (rv_eq (rvsqr (rvplus (fun r : R => T n (X n r)) (Y n))) 
+   assert (eqq1:rv_eq (rvsqr (rvplus (fun r : R => T n (X n r)) (Y n))) 
                  (rvplus (rvsqr (fun r : R => T n (X n r)))
                          (rvplus
                             (rvscale 2 (rvmult (fun r : R => T n (X n r))
                                                (Y n)))
                             (rvsqr (Y n))))).
-   intros r.
-   unfold rvsqr, rvplus, rvscale, Rsqr, rvmult.
-   lra.
-   rewrite H2.
-   assert (rvsqr (rvplus (fun r : R => T n (X n r)) (Y n)) = 
-           (rvplus (rvsqr (fun r : R => T n (X n r)))
-            (rvplus (rvscale 2 (rvmult (fun r : R => T n (X n r)) (Y n))) (rvsqr (Y n))))).
-   admit.
-   rewrite H3.
-   
+   { intros r.
+     unfold rvsqr, rvplus, rvscale, Rsqr, rvmult.
+     lra.
+   }
+   rewrite (SimpleExpectation_transport _ eqq1).
+   apply Dvoretzky_rel00.
    
 Admitted.
    
