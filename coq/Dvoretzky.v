4a4d7ee670338826df121b9712fcef09de11628e
Barry M. Trager <bmt@us.ibm.com>
2020-11-13 08:26:28 -0500

    nearly finished generalization with theta

--- coq/Dvoretzky.v
+++ coq/Dvoretzky.v
@@ -192,10 +192,81 @@ Lemma Dvoretzky_rel (n:nat) (theta:R) (T X Y : nat -> R -> R) (F : nat -> R)
   (forall (n:nat), rv_eq (X (S n)) (rvplus (fun r => T n (X n r)) (Y n))) ->
   rv_eq (SimpleConditionalExpectation (Y n) (X n)) (const 0) ->
   Rle (SimpleExpectation (rvsqr (rvminus (X (S n)) (const theta)) ))
-      ((Rsqr (F n)) * SimpleExpectation (rvsqr (X n))
-       + SimpleExpectation (rvsqr (rvminus (Y n) (const theta)))).
+      ((Rsqr (F n)) * SimpleExpectation (rvsqr (rvminus (X n) (const (theta))))
+       + SimpleExpectation (rvsqr (Y n))).
   Proof.
     intros.
     specialize (H1 n).
+    assert (rv_eq (rvminus (X (S n)) (const theta)) 
+                  (rvminus (rvplus (fun r => T n (X n r)) (Y n)) (const theta))).
+    now rewrite H1.
     rewrite (SimpleExpectation_transport (srvsqr (rvminus (X (S n)) (const theta)))
-                                        (rvsqr_proper _ _ H1)).    
+                                        (rvsqr_proper _ _ H3)).    
+   assert (eqq1:rv_eq (rvsqr (rvminus (rvplus (fun r : R => T n (X n r)) (Y n)) (const theta))) 
+                      (rvplus (rvsqr (rvminus (fun r : R => T n (X n r)) (const theta)))
+                              (rvplus
+                                 (rvscale 2 (rvmult (rvminus (fun r : R => T n (X n r)) (const theta))
+                                                    (Y n)))
+                            (rvsqr (Y n))))).
+   { intros r.
+     unfold rvsqr, rvplus, rvminus, rvopp, rvscale, Rsqr, rvmult, const.
+     unfold rvplus.
+     lra.
+   }
+   rewrite (SimpleExpectation_transport _ eqq1).
+   rewrite (SimpleExpectation_pf_irrel _ _).
+   rewrite <- sumSimpleExpectation; try typeclasses eauto.
+   rewrite <- sumSimpleExpectation; try typeclasses eauto.
+   rewrite <- scaleSimpleExpectation.
+   rewrite <- Rplus_assoc.
+   apply Rplus_le_compat_r.
+   generalize (conditional_tower_law (rvmult (rvminus (fun r : R => T n (X n r)) (const theta))
+                                             (Y n)) 
+                                     (X n)) ; intros tower.
+   generalize (conditional_scale_measurable (rvminus (fun r:R => T n (X n r)) (const theta))
+                                            (Y n) (X n)); intros cond_scale.
+   cut_to cond_scale.
+   - specialize (tower (rvmult_rv _ (Y n)) rvx).
+     specialize (tower (srvmult _ (Y n)) svx).
+     rewrite <- tower.
+     rewrite (SimpleExpectation_transport _ cond_scale).
+     assert (eqq4:rv_eq  (rvmult (rvminus (fun r : R => T n (X n r)) (const theta))
+                                 (SimpleConditionalExpectation (Y n) (X n)))
+                         (const 0)).
+     {
+       rewrite H2.
+       unfold rvmult, const; intros ?; simpl; field.
+     } 
+     rewrite (SimpleExpectation_transport _ eqq4).
+     rewrite SimpleExpectation_const.
+     rewrite Rmult_0_r, Rplus_0_r.
+     specialize (H n).
+     rewrite (scaleSimpleExpectation (Rsqr (F n))).
+     
+     apply SimpleExpectation_le; try typeclasses eauto.
+     unfold RealRandomVariable_le.
+     intros.
+     unfold rvsqr, rvscale.
+     specialize (H0 n (X n x)).
+     rewrite <- Rabs_right with (r:=F n) in H0; trivial.
+     rewrite <- Rabs_mult in H0.
+     apply Rsqr_le_abs_1 in H0.
+     rewrite Rsqr_mult in H0.
+     unfold rvminus, rvopp, rvplus, rvscale, const.
+     unfold Rminus in H0.
+     replace (-1 * theta) with (-theta) by lra.
+     apply H0.
+   - unfold simple_sigma_measurable.
+     unfold event_preimage, singleton_event.
+     destruct svx.
+     destruct svt.
+     unfold RandomVariable.srv_vals.
+
+     
+     
+     
+     
+   
+   
+
+
