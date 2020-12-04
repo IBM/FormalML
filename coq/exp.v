0c5483bb34ac5b01088680d5c13e4c2a3db25bef
Barry M. Trager <bmt@us.ibm.com>
2020-12-04 13:55:23 -0500

    cleanup

--- coq/exp.v
+++ coq/exp.v
@@ -10,32 +10,24 @@ Proof.
 Qed.
 
 Lemma exp_ineq (x : R) : 1 + x <= exp x.  
-  generalize (exp_pos); intros.
-  generalize (MVT_cor2 (fun x=> exp x - (x + 1)) (fun x => exp x - 1)); intros.
   destruct (Rlt_dec 0 x).
   - left; now apply exp_ineq1.
   - destruct (Rlt_dec x 0).
-    + specialize (H0 x 0 r).
-      assert (forall c : R,
-                 x <= c <= 0 -> 
-                 derivable_pt_lim (fun x : R => exp x - (x + 1)) c (exp c - 1)); intros.
-      * apply derivable_pt_lim_minus.
-        -- apply derivable_pt_lim_exp.
-        -- replace (1) with (1 + 0) at 1 by lra.
-           apply derivable_pt_lim_plus.
-           ++ apply derivable_pt_lim_id.
-           ++ apply derivable_pt_lim_const.
-     * specialize (H0 H1).
-       destruct H0 as [c [? ?]].
-       rewrite exp_0 in H0; ring_simplify in H0.
+    + generalize (MVT_cor2 (fun x => exp x - (x + 1)) (fun x => exp x - 1) x 0 r); intro MVT.
+      destruct MVT as [c [? ?]].
+      * intros.
+        apply derivable_pt_lim_minus; [apply derivable_pt_lim_exp | ].
+        replace (1) with (1 + 0) at 1 by lra.
+        apply derivable_pt_lim_plus; 
+          [apply derivable_pt_lim_id | apply derivable_pt_lim_const].
+     * rewrite exp_0 in H; ring_simplify in H.
        apply Rge_le; apply Rminus_ge.
-       apply Ropp_eq_compat in H0; ring_simplify in H0.
-       ring_simplify; rewrite H0; left.
+       apply Ropp_eq_compat in H; ring_simplify in H.
+       ring_simplify; rewrite H; left.
        replace (x * exp c - x) with ((-x)*(1-exp c)) by lra.
        apply Rmult_lt_0_compat.
        -- lra.
-       -- destruct H2.
-          apply Rgt_minus.
+       -- apply Rgt_minus.
           replace (c) with (- - c) by lra.
           rewrite exp_Ropp.
           replace (1) with (/ 1) by lra.
