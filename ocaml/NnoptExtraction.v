9b0aac4e7352ba4af7eceb3f8cada5331d57f034
Avi Shinnar <shinnar@us.ibm.com>
2020-12-10 22:41:01 -0500

    Workaround for bug in Coq extraction

--- ocaml/NnoptExtraction.v
+++ ocaml/NnoptExtraction.v
@@ -62,5 +62,11 @@ Module API.
   
   End API.
 
+(* Workaround for https://github.com/coq/coq/issues/13288 , suggested by a comment on the issue.  
+ Coq extraction currently creates a clash between the extracted Decimal.int and the 
+ ocaml int type.
+*)
+Extract Inductive Decimal.int => unit [ "(fun _ -> ())" "(fun _ -> ())" ] "(fun _ _ _ -> assert false)".
+
 Extraction "extracted/NnoptExtracted" API.
 
