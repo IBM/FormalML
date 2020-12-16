e57103bc3ca795dc20d91cecb6d86b36ead9a403
Avi Shinnar <shinnar@us.ibm.com>
2020-12-15 21:00:36 -0500

    start work on showing the L2 space is a hilbert space

---
+++ coq/RandomVariableL2.v
@@ -0,0 +1,353 @@
+Require Import Morphisms.
+Require Import Equivalence.
+Require Import Program.Basics.
+Require Import Lra.
+Require Import Classical.
+Require Import FunctionalExtensionality.
+
+Require Import hilbert.
+
+Require Import BorelSigmaAlgebra.
+Require Import ProbSpace.
+Require Import RandomVariable.
+Require Import quotient_space.
+
+Require Import AlmostEqual.
+
+Set Bullet Behavior "Strict Subproofs".
+
+Section L2.
+  Context {Ts:Type} 
+          {dom: SigmaAlgebra Ts}
+          (prts: ProbSpace dom).
+
+
+  Class IsL2 (rv_X:Ts->R) {rv:RandomVariable prts borel_sa rv_X}
+    := is_L2 : match Expectation (rvsqr rv_X) with
+       | Some (Finite _) => True
+       | _ => False
+       end.
+
+  Instance is_L2_const x : IsL2 (const x).
+  Proof.
+  Admitted.
+
+  Instance is_L2_plus rv_X1 rv_X2
+           {rv1:RandomVariable prts borel_sa rv_X1}
+           {rv2:RandomVariable prts borel_sa rv_X2}
+           {isl21:IsL2 rv_X1}
+           {isl22:IsL2 rv_X2} :
+    IsL2 (rvplus rv_X1 rv_X2).
+  Proof.
+  Admitted.
+
+  Instance is_L2_scale x rv_X {rv:RandomVariable prts borel_sa rv_X}
+           {isl2:IsL2 rv_X} :
+    IsL2 (rvscale x rv_X).
+  Proof.
+  Admitted.
+
+  
+  Record L2RRV : Type
+    := L2RRV_of {
+      L2RRV_rv_X :> Ts -> R
+      ; L2RRV_rv :> RandomVariable prts borel_sa L2RRV_rv_X
+      ; L2RRV_l2 :> IsL2 L2RRV_rv_X
+         }.
+
+  Existing Instance L2RRV_rv.
+  Existing Instance L2RRV_l2.
+  
+  Definition pack_L2RRV (rv_X:Ts -> R) {rv:RandomVariable prts borel_sa rv_X} {l2:IsL2 rv_X}
+    := L2RRV_of rv_X rv l2.
+  
+  Definition L2RRV_eq (rv1 rv2:L2RRV)
+    := rv_almost_eq prts rv1 rv2.
+
+  Local Hint Resolve Hsigma_borel_eq_pf : prob.
+
+  Global Instance L2RRV_eq_equiv : Equivalence L2RRV_eq.
+  Proof.
+    unfold L2RRV_eq, rv_almost_eq.
+    constructor; red.
+    - intros [x?].
+      assert (eqq:event_equiv (fun x0 : Ts => x x0 = x x0) Ω)
+        by firstorder.
+      rewrite eqq.
+      eauto with prob.
+    - intros [x?] [y?] ps1; simpl in *.
+      rewrite ps_proper; try eassumption.
+      red; intros.
+      split; congruence.
+    - intros [x??] [y??] [z??] ps1 ps2.
+      simpl in *.
+      rewrite rv_almost_eq_alt_eq in ps1 by eauto with prob.
+      rewrite rv_almost_eq_alt_eq in ps2 by eauto with prob.
+      rewrite rv_almost_eq_alt_eq by eauto with prob.
+      generalize (ps_union prts _ _ (sa_complement _ (Hsigma_borel_eq_pf _ x y _ _)) (sa_complement _ (Hsigma_borel_eq_pf _ y z _ _)))
+      ; intros HH.
+      unfold event_complement in HH.
+      rewrite ps1,ps2 in HH.
+      field_simplify in HH.
+
+      assert (HH2 : ps_P
+                      (event_inter (event_complement (fun x0 : Ts => x x0 = y x0))
+                                   (event_complement (fun x : Ts => y x = z x))) = 0).
+      {
+        assert (HH3:ps_P
+                      (event_union (event_complement (fun x0 : Ts => x x0 = y x0))
+                                   (event_complement (fun x : Ts => y x = z x))) 
+                    +
+                    ps_P
+                      (event_inter (event_complement (fun x0 : Ts => x x0 = y x0))
+                                   (event_complement (fun x : Ts => y x = z x))) = 0)
+          by (unfold event_complement; lra).
+        rewrite Rplus_comm in HH3.
+        apply Rplus_eq_0_l in HH3; trivial
+        ; apply ps_pos
+        ; eauto 6 with prob.
+      }
+      unfold event_complement in HH2.
+      rewrite HH2, Ropp_0 in HH.
+      unfold event_union in HH2.
+
+      assert (ele:event_sub
+                    (event_complement (fun x0 : Ts => x x0 = z x0))
+                    (event_union (event_complement (fun x1 : Ts => x x1 = y x1))
+                                 (event_complement (fun x : Ts => y x = z x)))).
+      {
+        unfold event_complement.
+        red; intros.
+        apply not_and_or.
+        intros [??].
+        congruence.
+      }
+
+      apply (@ps_sub _ _ prts) in ele; trivial.
+      * unfold event_complement in ele.
+        rewrite HH in ele.
+        apply Rle_antisym; trivial.
+        apply ps_pos; trivial.
+        apply sa_complement.
+        eauto with prob.
+      * eauto with prob.
+      * apply sa_union; eauto with prob.
+  Qed.
+  
+  Definition L2RRVconst (x:R) : L2RRV
+    := pack_L2RRV (const x).
+
+  Definition L2RRVzero : L2RRV := L2RRVconst 0.
+
+
+  Definition L2RRVplus (rv1 rv2:L2RRV) : L2RRV
+    := pack_L2RRV (rvplus rv1  rv2).
+
+  Global Instance L2RRV_plus_proper : Proper (L2RRV_eq ==> L2RRV_eq ==> L2RRV_eq) L2RRVplus.
+  Proof.
+    unfold Proper, respectful, L2RRV_eq.
+    intros [x1??] [x2??] eqqx [y1??] [y2??] eqqy.
+    simpl in *.
+    unfold rv_almost_eq in *.
+  Admitted.
+
+  
+  Program Definition L2RRVscale (x:R) (rv:L2RRV) : L2RRV
+    := pack_L2RRV (rvscale x rv).
+
+  Global Instance L2RRV_scale_proper : Proper (eq ==> L2RRV_eq ==> L2RRV_eq) L2RRVscale.
+  Proof.
+    unfold Proper, respectful, L2RRV_eq.
+    intros ? x ? [x1??] [x2??] eqqx.
+    subst.
+    simpl in *.
+    unfold rvscale.
+    red.
+    destruct (Req_EM_T x 0).
+    - subst.
+      erewrite ps_proper; try eapply ps_one.
+      red.
+      unfold Ω.
+      split; trivial.
+      lra.
+    - erewrite ps_proper; try eapply eqqx.
+      red; intros.
+      split; intros.
+      + eapply Rmult_eq_reg_l; eauto.
+      + congruence.
+  Qed.
+
+  Instance is_L2_opp rv_X
+           {rv:RandomVariable prts borel_sa rv_X}
+           {isl2:IsL2 rv_X} :
+    IsL2 (rvopp rv_X).
+  Proof.
+    unfold rvopp.
+    eapply is_L2_scale; eauto.
+  Qed.
+
+  Program Definition L2RRVopp (rv:L2RRV) : L2RRV
+    := pack_L2RRV (rvopp rv).
+  
+  Global Instance L2RRV_opp_proper : Proper (L2RRV_eq ==> L2RRV_eq) L2RRVopp.
+  Proof.
+    unfold Proper, respectful.
+    intros x y eqq.
+    generalize (L2RRV_scale_proper (-1) _ (eq_refl _) _ _ eqq)
+    ; intros HH.
+    destruct x as [x?]
+    ; destruct y as [y?].
+    apply HH.
+  Qed.
+  
+  Definition L2RRVminus (rv1 rv2:L2RRV) : L2RRV
+    := pack_L2RRV (rvminus rv1 rv2).
+
+  Global Instance L2RRV_minus_proper : Proper (L2RRV_eq ==> L2RRV_eq ==> L2RRV_eq) L2RRVminus.
+  Proof.
+    unfold Proper, respectful, L2RRV_eq.
+
+    intros x1 x2 eqq1 y1 y2 eqq2.
+    
+    generalize (L2RRV_plus_proper _ _ eqq1 _ _ (L2RRV_opp_proper _ _ eqq2)) 
+    ; intros HH.
+    destruct x1 as [???]; destruct x2 as [???]
+    ; destruct y1 as [???]; destruct y2 as [???].
+    apply HH.
+  Qed.
+
+  Ltac L2RRV_simpl
+    := repeat match goal with
+              | [H : L2RRV |- _ ] => destruct H as [???]
+              end
+       ; unfold L2RRVplus, L2RRVminus, L2RRVopp, L2RRVscale
+       ; simpl.
+
+  Lemma L2RRV_plus_comm x y : L2RRV_eq (L2RRVplus x y) (L2RRVplus y x).
+  Proof.
+    red; intros.
+    L2RRV_simpl.
+    apply rv_almost_eq_eq; intros ?.
+    unfold rvplus; lra.
+  Qed.
+  
+  Lemma L2RRV_plus_assoc (x y z : L2RRV) : L2RRV_eq (L2RRVplus x (L2RRVplus y z)) (L2RRVplus (L2RRVplus x y) z).
+  Proof.
+    red; intros.
+    L2RRV_simpl.
+    apply rv_almost_eq_eq; intros ?.
+    unfold rvplus.
+    lra.
+  Qed.
+
+  Lemma L2RRV_plus_zero (x : L2RRV) : L2RRV_eq (L2RRVplus x (L2RRVconst 0)) x.
+  Proof.
+    red; intros.
+    L2RRV_simpl.
+    apply rv_almost_eq_eq; intros ?.
+    unfold rvplus, const.
+    lra.
+  Qed.
+
+  Lemma L2RRV_plus_inv (x: L2RRV) : L2RRV_eq (L2RRVplus x (L2RRVopp x)) (L2RRVconst 0).
+  Proof.
+    red; intros.
+    L2RRV_simpl.
+    apply rv_almost_eq_eq; intros ?.
+    unfold rvplus, rvopp, rvscale, const.
+    lra.
+  Qed.
+  
+  Definition L2RRVq : Type := quot L2RRV_eq.
+
+  Definition L2RRVq_const (x:R) : L2RRVq := Quot _ (L2RRVconst x).
+
+  Lemma L2RRVq_constE x : L2RRVq_const x = Quot _ (L2RRVconst x).
+  Proof.
+    reflexivity.
+  Qed.
+
+  Hint Rewrite L2RRVq_constE : quot.
+
+  Definition L2RRVq_zero : L2RRVq := L2RRVq_const 0.
+
+  Lemma L2RRVq_zeroE : L2RRVq_zero = L2RRVq_const 0.
+  Proof.
+    reflexivity.
+  Qed.
+
+  Hint Rewrite L2RRVq_zeroE : quot.
+  
+  Definition L2RRVq_opp  : L2RRVq -> L2RRVq
+    := quot_lift _ L2RRVopp.
+
+  Lemma L2RRVq_oppE x : L2RRVq_opp (Quot _ x)  = Quot _ (L2RRVopp x).
+  Proof.
+    apply quot_liftE.
+  Qed.
+
+  Hint Rewrite L2RRVq_oppE : quot.
+
+  Definition L2RRVq_plus  : L2RRVq -> L2RRVq -> L2RRVq
+    := quot_lift2 _ L2RRVplus.
+  
+  Lemma L2RRVq_plusE x y : L2RRVq_plus (Quot _ x) (Quot _ y) = Quot _ (L2RRVplus x y).
+  Proof.
+    apply quot_lift2E.
+  Qed.
+
+  Hint Rewrite L2RRVq_plusE : quot.
+
+  Definition L2RRVq_minus  : L2RRVq -> L2RRVq -> L2RRVq
+    := quot_lift2 _ L2RRVminus.
+
+  Lemma L2RRVq_minusE x y : L2RRVq_minus (Quot _ x) (Quot _ y) = Quot _ (L2RRVminus x y).
+  Proof.
+    apply quot_lift2E.
+  Qed.
+
+  Hint Rewrite L2RRVq_minusE : quot.
+
+
+  Ltac L2RRVq_simpl
+    := repeat match goal with
+       | [H: L2RRVq |- _ ] =>
+         let xx := fresh H in destruct (Quot_inv H) as [xx ?]; subst H; rename xx into H
+              end
+       ; try autorewrite with quot
+       ; try apply (@eq_Quot _ _ L2RRV_eq_equiv).
+
+  Lemma L2RRVq_plus_comm x y : L2RRVq_plus x y = L2RRVq_plus y x.
+  Proof.
+    L2RRVq_simpl.
+    apply L2RRV_plus_comm.
+  Qed.
+         
+  Lemma L2RRVq_plus_assoc (x y z : L2RRVq) : L2RRVq_plus x (L2RRVq_plus y z) = L2RRVq_plus (L2RRVq_plus x y) z.
+  Proof.
+    L2RRVq_simpl.
+    apply L2RRV_plus_assoc.
+  Qed.
+
+
+  Lemma L2RRVq_plus_zero (x : L2RRVq) : L2RRVq_plus x L2RRVq_zero = x.
+  Proof.
+    L2RRVq_simpl.
+    apply L2RRV_plus_zero.
+  Qed.
+
+  Lemma L2RRVq_plus_inv (x: L2RRVq) : L2RRVq_plus x (L2RRVq_opp x) = L2RRVq_zero.
+  Proof.
+    L2RRVq_simpl.
+    apply L2RRV_plus_inv.
+  Qed.
+    
+  Program Definition L2RRVq_AbelianGroup_mixin : AbelianGroup.mixin_of L2RRVq
+    := AbelianGroup.Mixin L2RRVq L2RRVq_plus L2RRVq_opp L2RRVq_zero
+                          L2RRVq_plus_comm L2RRVq_plus_assoc
+                          L2RRVq_plus_zero L2RRVq_plus_inv.
+
+  Canonical L2RRVq_AbelianGroup :=
+    AbelianGroup.Pack L2RRVq L2RRVq_AbelianGroup_mixin L2RRVq.
+
+End L2.
