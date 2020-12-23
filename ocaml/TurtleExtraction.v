b31b5a9db251e9499a142d6f1ddd30f9a4bab1c3
Avi Shinnar <shinnar@us.ibm.com>
2020-12-22 20:40:10 -0500

    WIP

--- ocaml/TurtleExtraction.v
+++ ocaml/TurtleExtraction.v
@@ -5,11 +5,41 @@ Extraction Language OCaml.
 Require Import ExtrOcamlBasic ExtrOcamlString ExtrOcamlZInt ExtrOcamlNatInt.
 Extraction Blacklist String List.
 
-Require Import mdp_turtle.
+Require Import Reals.
+Require LibUtilsCoqLibAdd.
+Require String.
+Require mdp_turtle mdp.
 Local Open Scope list.
 
 Module API.
 
+  Definition MDP : Type := mdp.MDP.
+  Definition Grid (m n :nat) : Type := mdp_turtle.turtle_grid m n.
+
+  Definition turtle_color := mdp_turtle.turtle_color.
+  
+  Definition turtle_white := mdp_turtle.turtle_white.
+  Definition turtle_green:= mdp_turtle.turtle_green.
+  Definition turtle_star := mdp_turtle.turtle_star.
+  Definition turtle_red := mdp_turtle.turtle_red.
+  
+  Definition turtle_mdp (m n:nat) {g:Grid (S m) (S n)} : MDP := mdp_turtle.turtle_mdp g.
+
+  Definition inner_length {A} (g:list (list A))
+    := match g with
+       | nil => 0
+       | x::_ => length x
+       end.
+      
+  Definition make_grid (g:list (list turtle_color)) : option (Grid (length g) (inner_length g)).
+  Admitted.
+
+  Definition CeRtL_mdp : MDP :=  mdp_turtle.CeRtL_mdp.
+
+  Definition show_grid (m n:nat) {g:Grid m n} : String.string
+    := LibUtilsCoqLibAdd.toString (ToString:=mdp_turtle.turtle_grid_tostring) g.
+
+  Definition CeRtL_run_optimal (γ : R) (approx_iters:nat) (steps:nat) : String.string := mdp_turtle.CeRtL_run_optimal γ approx_iters steps.
 
 End API.
 
