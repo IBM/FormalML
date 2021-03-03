3575b25ee981b8f1186b554f8331db38e7d8d2c1
Avi Shinnar <shinnar@us.ibm.com>
2021-03-03 16:50:22 -0500

    Prove is_partition_refine

--- coq/converge/qlearn.v
+++ coq/converge/qlearn.v
@@ -1974,22 +1974,132 @@ algorithm.
     :=                                                   
       refine_dec_sa_partitions (vec_induced_sigma_generators srv) l.
 
+
+  Lemma ForallOrdPairs_app {A : Type} {R : A -> A -> Prop} {l1 l2 : list A} :
+    ForallOrdPairs R l1 ->
+    ForallOrdPairs R l2 ->
+    (forall x y, In x l1 -> In y l2 -> R x y) ->
+    ForallOrdPairs R (l1 ++ l2).
+  Proof.
+    revert l2.
+    induction l1; simpl; trivial; intros.
+    invcs H.
+    constructor.
+    - apply Forall_app; trivial.
+      rewrite Forall_forall.
+      intros.
+      apply H1; trivial.
+      eauto.
+    - apply IHl1; trivial.
+      eauto.
+  Qed.
+
+  Lemma events_disjoint_refine_event (a : dec_sa_event) (l : list dec_sa_event) :
+    ForallOrdPairs event_disjoint (map dsa_event l) ->
+    ForallOrdPairs event_disjoint (map dsa_event (refine_dec_sa_event a l)).
+  Proof.
+    induction l; simpl; trivial; intros F1.
+    invcs F1.
+    constructor; [| auto].
+    rewrite Forall_forall; intros ? inn.
+    unfold refine_dec_sa_event, dec_sa_event_inter in inn.
+    rewrite map_map in inn.
+    simpl in inn.
+    apply in_map_iff in inn.
+    destruct inn as [? [??]]; subst.
+    unfold event_disjoint, event_inter; intros.
+    rewrite Forall_forall in H1.
+    destruct H; destruct H3.
+    eapply (H1 (dsa_event x1)).
+    - apply in_map_iff; eauto.
+    - eauto.
+    - eauto.
+  Qed.
+
+  Lemma events_disjoint_refine (l1 l2 : list dec_sa_event) :
+    ForallOrdPairs event_disjoint (map dsa_event l1) ->
+    ForallOrdPairs event_disjoint (map dsa_event l2) ->
+    ForallOrdPairs event_disjoint
+                   (map dsa_event (flat_map (fun e1 : dec_sa_event => refine_dec_sa_event e1 l2) l1)).
+  Proof.
+    revert l2.
+    induction l1; simpl; trivial.
+    intros l2 F1 F2.
+    rewrite map_app.
+    invcs F1.
+    apply ForallOrdPairs_app.
+    - now apply events_disjoint_refine_event.
+    - auto.
+    - intros x y xinn yinn.
+      unfold refine_dec_sa_event, dec_sa_event_inter in xinn, yinn.
+      rewrite map_map in xinn.
+      simpl in xinn.
+      apply in_map_iff in xinn.
+      destruct xinn as [?[??]]; subst.
+      apply in_map_iff in yinn.
+      destruct yinn as [?[??]]; subst.
+      apply in_flat_map in H3.
+      destruct H3 as [?[??]].
+      apply in_map_iff in H3.
+      destruct H3 as [?[??]]; subst.
+      simpl.
+      rewrite Forall_forall in H1.
+      specialize (H1 (dsa_event x2)).
+      cut_to H1.
+      + firstorder.
+      + apply in_map_iff; eauto.
+  Qed.        
+
+  Lemma event_equiv_list_union_refine_event a l :
+    event_equiv (list_union (map dsa_event l)) Ω ->
+    event_equiv (list_union (map dsa_event (refine_dec_sa_event a l))) (dsa_event a).
+  Proof.
+    unfold event_equiv, refine_dec_sa_event, list_union, dec_sa_event_inter; intros.
+    rewrite map_map; simpl.
+    split; intros.
+    - destruct H0 as [?[??]].
+      apply in_map_iff in H0.
+      destruct H0 as [?[??]]; subst.
+      firstorder.
+    - destruct (H x).
+      cut_to H2; [| now red].
+      destruct H2 as [? [??]].
+      apply in_map_iff in H2.
+      destruct H2 as [?[??]]; subst.
+      exists (event_inter (dsa_event a) (dsa_event x2)).
+      split.
+      + apply in_map_iff.
+        eauto.
+      + red; tauto.
+  Qed.
+
+  Lemma event_equiv_list_union_refine_all l1 l2 :
+    event_equiv (list_union (map dsa_event l2)) Ω ->
+    event_equiv
+      (list_union (map dsa_event (flat_map (fun e1 : dec_sa_event => refine_dec_sa_event e1 l2) l1)))
+      (list_union (map dsa_event l1)).
+  Proof.
+    revert l2.
+    induction l1; simpl; trivial; intros l2 E.
+    - reflexivity.
+    - rewrite map_app.
+      rewrite list_union_app, list_union_cons.
+      apply event_union_proper.
+      + now apply event_equiv_list_union_refine_event.
+      + now apply IHl1.
+  Qed.
+
   Lemma is_partition_refine (l1 l2 : list dec_sa_event) :
     is_partition_list (map dsa_event l1) ->
     is_partition_list (map dsa_event l2) ->    
     is_partition_list (map dsa_event (refine_dec_sa_partitions l1 l2)).
   Proof.
     unfold is_partition_list, refine_dec_sa_partitions.
-    intros.
-    destruct H.
-    destruct H0.
+    intros [??] [??].
     split.
-    - induction l1.
-      + simpl.
-        apply FOP_nil.
-      + simpl.
-        rewrite map_app.
-    Admitted.
+    - now apply events_disjoint_refine.
+    - now rewrite event_equiv_list_union_refine_all.
+  Qed.
 
   Lemma is_partition_vec_induced_gen
           {rv_X : X -> X}
