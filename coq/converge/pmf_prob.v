706ed0d44030a3231c94ea46cd10bc059bf477b0
Avi Shinnar <shinnar@us.ibm.com>
2021-03-31 16:36:53 -0400

    converting ps_pmf to be a discrete_space

--- coq/converge/pmf_prob.v
+++ coq/converge/pmf_prob.v
@@ -1,7 +1,7 @@
-Require Import Reals Lra List.
+Require Import Reals Lra List Permutation.
 
 Require Import LibUtils utils.Utils.
-Require Import ProbSpace SigmaAlgebras.
+Require Import ProbSpace DiscreteProbSpace SigmaAlgebras.
 Require Import pmf_monad mdp.
 Require Import RealRandomVariable SimpleExpectation.
 Require Import cond_expt.
@@ -22,99 +22,312 @@ Proof.
   now destruct a.
 Qed.
 
-Section pmf_prob.
-  Context {A:Type}.
-  Context (pmf:Pmf A).
+Lemma seqmap_map {A B:Type} (f:A->B) (l:list A) : seq.map f l = List.map f l.
+Proof.
+  induction l; auto.
+Qed.
 
-  Definition sa_pmf : SigmaAlgebra A
-      := generated_sa (fun e => In e (map pre_event_singleton (map snd pmf.(outcomes)))).
+Section Pmf_PMF.
 
-  Program Definition ps_p_pmf (e:pre_event A) : R
-    := list_sum
-         (map (fun '(r, v) =>
-                 if excluded_middle_informative (e v)
-                 then nonneg r
-                 else 0%R)
-              pmf.(outcomes)).
+  Definition pmf_PMF_fun {A:Type} {countableA:Countable A} {dec:EqDec A eq} (pmf:Pmf A): A -> R
+    := fun a => list_sum
+               (map
+                  (fun x => nonneg (fst x))
+                  (filter
+                     (fun x => if a == snd x then true else false)
+                     (pmf.(outcomes)))).
 
-  Global Instance ps_p_pmf_proper : Proper (pre_event_equiv ==> eq) ps_p_pmf.
+  Lemma list_sum_nsubl0 l1 l2 :
+    (forall x, In x l2 /\ ~ In x l1 -> x = 0) ->
+    sublist l1 l2 ->
+    NoDup l2 ->
+    list_sum l1 = list_sum l2.
   Proof.
-    intros ?? eqq.
-    unfold ps_p_pmf.
-    f_equal.
-    apply map_ext; intros [??].
-   red in eqq.
-   repeat match_destr.
-   - apply eqq in x0; tauto.
-   - apply eqq in y0; tauto.
+    intros.
+    destruct (sublist_perm_head H0) as [l3 perm].
+    rewrite <- perm.
+    rewrite list_sum_cat.
+    assert (forall x, In x l3 -> x = 0).
+    {
+      intros ? inn.
+      apply H.
+      split.
+      - rewrite <- perm.
+        rewrite in_app_iff; eauto.
+      - intros inn2.
+        rewrite <- perm in H1.
+        apply NoDup_app_disj in H1.
+        eelim H1; eauto.
+    } 
+    cut (list_sum l3 = 0); [lra |].
+    clear perm.
+    induction l3; simpl in *; trivial.
+    rewrite IHl3 by eauto.
+    rewrite (H2 a) by eauto.
+    lra.
   Qed.
 
- Program Instance ps_pmf : ProbSpace sa_pmf
-   := {|
-   ps_P e := ps_p_pmf e
-     |}.
- Next Obligation.
-   intros ?? eqq1.
-   now apply ps_p_pmf_proper.
- Qed.
- Next Obligation.
-   unfold sum_of_probs_equals.
-   rewrite (infinite_sum'_ext _ (fun n : nat => ps_p_pmf (collection_pre collection n)))
-     by reflexivity.
-   apply -> (collection_is_pairwise_disjoint_pre collection) in H.
-   rewrite (ps_p_pmf_proper _ (fun t : A => exists n : nat, (collection_pre collection n) t)) by reflexivity.
-   revert H.
-   generalize (collection_pre collection); clear collection; intros collection.
-   intros.
-   unfold ps_p_pmf; simpl in *.
-   (* there should be a more constructive proof, but my spirit is broken *)
-   destruct pmf.
-   simpl in *.
-   clear sum1.
-   induction outcomes.
-   - red; simpl.
-     apply infinite_sum'0.
-   - simpl.
-     destruct a; simpl in *.
-     match_destr.
-     + destruct e as [nn Hn].
-       apply infinite_sum'_plus; trivial.
-       apply (infinite_sum'_one
-                     _
-                     nn n).
-       * intros.
-         match_destr.
-         eelim H; eauto.
-       * match_destr.
-         congruence.
-     + rewrite Rplus_0_l.
-       red.
-       eapply infinite_sum'_ext; try eapply IHoutcomes
-       ; intros; simpl.
-       match_destr; simpl.
-       * elim n0.
-         eexists; eauto.
-       * lra.
-Qed.
- Next Obligation.
-   unfold ps_p_pmf.
-   rewrite <- pmf.(sum1).
-   rewrite list_fst_sum_eq.
-   f_equal.
-   apply map_ext; intros [??].
-   match_destr.
-   elim n0; now red.
- Qed.
- Next Obligation.
-   unfold ps_p_pmf.
-   apply list_sum_pos_pos'.
-   rewrite Forall_forall; intros ? inn.
-   apply in_map_iff in inn.
-   destruct inn as [[??][??]].
-   match_destr_in H; subst.
-   - apply cond_nonneg.
-   - now right.
- Qed.
+  Lemma list_sum_nincl0 l1 l2 :
+    NoDup l1 ->
+    NoDup l2 ->
+    (forall x, In x l2 /\ ~ In x l1 -> x = 0) ->
+    incl l1 l2 ->
+    list_sum l1 = list_sum l2.
+  Proof.
+    intros.
+    destruct (incl_NoDup_sublist_perm H H2)
+      as [l3 [perm subl]].
+    rewrite perm.
+    apply list_sum_nsubl0; trivial.
+    intros ? [??].
+    apply (H1 x).
+    split; trivial.
+    now rewrite perm.
+  Qed.
+
+  Lemma list_sum_filter0 l : list_sum l = list_sum (filter (fun x => nequiv_decb x 0) l).
+  Proof.
+    induction l; simpl; trivial.
+    rewrite IHl; simpl.
+    unfold nequiv_decb, negb, equiv_decb.
+    destruct (equiv_dec a 0); simpl; trivial.
+    rewrite e; lra.
+  Qed.
+  
+  Lemma infinite_sum'_finite (coll:nat->R) (l:list nat):
+    (forall n, ~ In n l -> coll n = 0) ->
+    NoDup l ->
+    infinite_sum' coll (list_sum (map coll l)).
+  Proof.
+    intros nin nd.
+    apply (infinite_sum'_split (list_max l + 1)).
+    rewrite (infinite_sum'_ext _ (fun _ => 0)).
+    - replace (sum_f_R0' coll (list_max l + 1))
+        with (list_sum (map coll l)).
+      + rewrite Rminus_eq_0.
+        apply infinite_sum'0.
+      + rewrite sum_f_R0'_list_sum.
+        rewrite (list_sum_filter0 (map coll l)).
+        rewrite (list_sum_filter0 (map coll (seq 0 (list_max l + 1)))).
+        repeat rewrite filter_map_swap.
+        apply list_sum_perm_eq.
+        apply Permutation_map.
+        apply NoDup_Permutation'.
+        * now apply NoDup_filter.
+        * apply NoDup_filter.
+          apply seq_NoDup.
+        * intros a.
+          repeat rewrite filter_In.
+          split; intros [inn non0]; split; trivial.
+          -- apply in_seq.
+             split; [lia |].
+             generalize (list_max_upper l).
+             rewrite Forall_forall; intros lmu.
+             specialize (lmu _ inn).
+             lia.
+          -- destruct (in_dec eq_nat_dec a l); trivial.
+             specialize (nin _ n).
+             unfold nequiv_decb, negb, equiv_decb in non0.
+             destruct (equiv_dec (coll a) 0); congruence.
+    - intros.
+      apply nin.
+      intros inn.
+      generalize (list_max_upper l).
+      rewrite Forall_forall; intros lmu.
+      specialize (lmu _ inn).
+      lia.
+  Qed.
+
+  Lemma list_sum0_0 l: Forall (fun x => x = 0) l -> list_sum l = 0.
+  Proof.
+    induction l; simpl; trivial.
+    intros FF; invcs FF.
+    rewrite IHl; trivial.
+    lra.
+  Qed.
+
+
+  Lemma map_filter_nodup_perm_aux {A B:Type} {dec:EqDec B eq} (f:A->B) l (b:B) :
+   Permutation
+    (concat
+       (map
+          (fun x : B =>
+           filter (fun x0 : A => if dec x (f x0) then true else false)
+                  l) (nodup dec (map f l)))) l.
+  Proof.
+    rewrite <- (nodup_hd_quotient _ b).
+    rewrite map_map.
+    rewrite quotient_map.
+    rewrite map_map.
+    etransitivity
+    ; [| eapply (unquotient_quotient (fun x y => eq (f x) (f y)) (equiv:=Equivalence_pullback _ _ _) (dec:=EqDec_pullback _ _) l)].
+
+    apply perm2_concat_perm.
+
+    transitivity
+    (map
+       (fun x : list A =>
+        filter (fun x0 : A => if dec (hd b (map f x)) (f x0) then true else false) (concat (quotient (fun x y : A => f x = f y) (equiv:=Equivalence_pullback _ _ _) (dec:=EqDec_pullback _ _) l)))
+       (quotient (fun x y : A => f x = f y) (equiv:=Equivalence_pullback _ _ _) (dec:=EqDec_pullback _ _) l)).
+    -
+      assert (HH:forall ll, Forall2 (Permutation (A:=A))
+    (map
+       (fun x : list A =>
+        filter (fun x0 : A => if dec (hd b (map f x)) (f x0) then true else false) l)
+       ll)
+    (map
+       (fun x : list A =>
+        filter (fun x0 : A => if dec (hd b (map f x)) (f x0) then true else false)
+          (concat (quotient (equiv:=Equivalence_pullback _ _ _) (dec:=EqDec_pullback _ _) (fun x0 y : A => f x0 = f y) l)))
+       ll)).
+      {
+        induction ll; simpl; trivial.
+        constructor; trivial.
+        apply Permutation_filter.
+        symmetry.
+        apply unquotient_quotient.
+      }
+      apply HH.
+    -
+      generalize (quotient_all_equivs (fun x y : A => f x = f y) (equiv:=Equivalence_pullback _ _ _) (dec:=EqDec_pullback _ _) l).
+      generalize (quotient_all_different  (fun x y : A => f x = f y) (equiv:=Equivalence_pullback _ _ _) (dec:=EqDec_pullback _ _) l).
+      generalize (quotient_nnil  (fun x y : A => f x = f y) (equiv:=Equivalence_pullback _ _ _) (dec:=EqDec_pullback _ _) l).
+      generalize (quotient (fun x y : A => f x = f y) (equiv:=Equivalence_pullback _ _ _) (dec:=EqDec_pullback _ _) l)
+      ; intros ll ll_nnil ll_diff ll_equivs.
+      induction ll; simpl; trivial.
+      invcs ll_nnil.
+      invcs ll_diff.
+      invcs ll_equivs.
+      constructor.
+      + rewrite filter_app.
+        replace (filter (fun x0 : A => if dec (hd b (map f a)) (f x0) then true else false) a) with a.
+        * replace (filter (fun x0 : A => if dec (hd b (map f a)) (f x0) then true else false) (concat ll)) with (@nil A).
+          -- rewrite app_nil_r; trivial.
+          -- symmetry.
+             apply false_filter_nil; intros.
+             match_destr.
+             red in e.
+             unfold not_nil in H1.
+             destruct a; [congruence |].
+             simpl in e.
+             apply in_concat in H.
+             destruct H as [?[inn1 inn2]].
+             rewrite Forall_forall in H3.
+             specialize (H3 _ inn1).
+             elim (H3 a x); simpl; eauto.
+        * symmetry.
+          apply true_filter; intros.
+          match_destr.
+          unfold not_nil in H1.
+          destruct a; [congruence |].
+          simpl in c.
+          elim c.
+          apply (H5 a x); simpl; eauto.
+      + etransitivity; [| eapply IHll]; trivial.
+        erewrite map_ext_in; [reflexivity |]; intros ? inn1.
+        rewrite filter_app.
+        replace (filter (fun x0 : A => if dec (hd b (map f a0)) (f x0) then true else false) a) with (@nil A); simpl; trivial.
+        symmetry.
+        apply false_filter_nil; intros ? inn2.
+        rewrite Forall_forall in *.
+        match_destr.
+        specialize (H2 _ inn1).
+        unfold not_nil in H2.
+        destruct a0; [congruence |].
+        simpl in e.
+        specialize (H3 _ inn1).
+        elim (H3 x a0); simpl; eauto.
+  Qed.
+
+  Lemma map_filter_nodup_perm {A B:Type} {dec:EqDec B eq} (f:A->B) l :
+    Permutation
+      (concat
+         (map
+            (fun x : B =>
+               filter (fun x0 : A => if dec x (f x0) then true else false)
+                      l) (nodup dec (map f l)))) l.
+  Proof.
+    destruct l; [reflexivity |].
+    apply map_filter_nodup_perm_aux.
+    exact (f a).
+  Qed.
+  
+  Program Definition pmf_PMF {A:Type} {countableA:Countable A} {dec:EqDec A eq} (pmf:Pmf A) : prob_mass_fun A
+    := {|
+    pmf_pmf := pmf_PMF_fun pmf
+      |}. 
+  Next Obligation.
+    unfold pmf_PMF_fun.
+    apply list_sum_pos_pos'.
+    apply Forall_map.
+    rewrite Forall_forall; intros [[??]?]; intros; simpl.
+    apply cond_nonneg.
+  Qed.
+  Next Obligation.
+    unfold pmf_PMF_fun, countable_sum.
+    replace 1 with R1 by lra.
+    destruct pmf; simpl.
+    rewrite <- sum1; clear sum1.
+    rewrite list_fst_sum_eq.
+    replace (list_sum (map (fun x : nonnegreal * A => nonneg (fst x)) outcomes))
+      with (list_sum (map (fun n : nat =>
+     match countable_inv n with
+     | Some a =>
+         list_sum
+           (map (fun x : nonnegreal * A => nonneg (fst x))
+              (filter (fun x : nonnegreal * A => if equiv_dec a (snd x) then true else false) outcomes))
+     | None => 0
+     end) (nodup eq_nat_dec (map countable_index (map snd outcomes))))).
+    - apply infinite_sum'_finite
+      ; intros.
+      + match_case; intros.
+        apply countable_inv_sound in H0.
+        apply list_sum0_0.
+        rewrite Forall_forall; intros ? inn.
+        apply in_map_iff in inn.
+        destruct inn as [[??][? inn]]; simpl in *; subst.
+        apply filter_In in inn.
+        destruct inn as [inn an0].
+        simpl in *.
+        match_destr_in an0.
+        red in e; subst.
+        elim H.
+        apply nodup_In.
+        apply in_map.
+        apply in_map_iff.
+        exists (n0, a0); simpl; eauto.
+      + apply NoDup_nodup.
+    - rewrite nodup_map_inj with (decA:=dec)
+      ; [| intros; now apply countable_index_inj].
+      rewrite map_map.
+      erewrite map_ext
+      ; [| intros; rewrite countable_inv_index; reflexivity].
+      rewrite <- map_map.
+      rewrite <- list_sum_map_concat.
+      transitivity (list_sum
+                      (concat
+                         (map (map (fun x0 : nonnegreal * A => nonneg (fst x0)))
+                              (map (fun x => (filter (fun x0 : nonnegreal * A => if equiv_dec x (snd x0) then true else false)
+                                                  outcomes)) (nodup dec (map snd outcomes)))))).
+      {
+        now rewrite map_map.
+      }
+      rewrite concat_map_map.
+      apply list_sum_perm_eq.
+      apply Permutation_map.
+      apply map_filter_nodup_perm.
+  Qed.
+
+End Pmf_PMF.
+
+Section pmf_prob.
+  Context {A:Type}.
+  Context {countableA:Countable A}.
+  Context {decA:EqDec A eq}.
+  Context (pmf:Pmf A).
+
+
+  Instance ps_pmf : ProbSpace (discrete_sa A) := discrete_ps (pmf_PMF pmf).
 
  Lemma pmf_SimpleExpectation_value_const (c:R) :
      SimpleExpectation (Prts:=ps_pmf) (const c) = expt_value pmf (const c).
@@ -125,7 +338,7 @@ Qed.
  Qed.
 
  Lemma pmf_SimpleExpectation_value_point_preimage_indicator (rv_X : A -> R) (c:R)
-       {rv:RandomVariable sa_pmf borel_sa rv_X} :
+       {rv:RandomVariable (discrete_sa A) borel_sa rv_X} :
    SimpleExpectation (Prts:=ps_pmf) (point_preimage_indicator rv_X c) =
    expt_value pmf (point_preimage_indicator rv_X c).
  Proof.
