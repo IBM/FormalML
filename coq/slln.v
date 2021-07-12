ad6f553366f1cfb8928b56f26a4567b724e8c748
Koundinya Vajjha <koundinya.vajjha@gmail.com>
2021-07-11 23:08:31 -0400

    More WIP Ash 6.1.1

--- coq/slln.v
+++ coq/slln.v
@@ -1,43 +1,48 @@
-(*Require Import Morphisms.
-Require Import Equivalence.
-Require Import Program.Basics.
-Require Import Classical.
-Require Import FunctionalExtensionality.
-Require Import IndefiniteDescription ClassicalDescription.
-
-Require Import hilbert.
-
-Require Export RandomVariableLpR.
-Require Import quotient_space.
-Require Import RbarExpectation.
-
-Require Import Event.
-Require Import AlmostEqual.
-Require Import utils.Utils.
-Require Import List.*)
-
-Require Import Lra Lia Reals Coquelicot.Coquelicot.
+Require Import Lra Lia Reals RealAdd RandomVariableL2 Coquelicot.Coquelicot.
+Require Import Sums.
 Set Bullet Behavior "Strict Subproofs".
 Set Default Goal Selector "!".
 
 
 Definition bounded (x : nat -> R) := exists c : R, forall n, Rabs (x n) <= c.
 
-Lemma Series_ex_series (a : nat -> R) : ex_series a -> (exists c, Series a <= c).
+Lemma sum_f_R0_Rabs_pos (x : nat -> R) : forall N, 0 <= sum_f_R0 (fun j => Rabs (x j)) N.
 Proof.
-  intros H.
-  destruct H as [c Hc].
-  exists (1 + Series a).
-  lra.
+  intros N.
+  rewrite sum_f_R0_sum_f_R0'.
+  apply sum_f_R0'_le.
+  intros. apply Rabs_pos.
 Qed.
 
-Lemma ash_6_1_1_a {x : nat -> R}{a : nat -> nat -> R} (ha : forall j, is_lim_seq (fun n => a n j) 0)
+Lemma sum_f_R0_nonneg_le_Series {x : nat -> R}(hx1 : ex_series x) (hx2 : forall n, 0 <= x n) :
+  forall N, sum_f_R0 x N <= Series x.
+Proof.
+  intros N.
+  unfold Series.
+  rewrite <-sum_n_Reals.
+  apply is_lim_seq_incr_compare.
+  + apply Lim_seq_correct'.
+    now rewrite ex_finite_lim_series.
+  + intros n.
+    apply sum_n_pos_incr; try lia; intros; auto.
+Qed.
+
+
+Lemma is_lim_seq_sum_f_R0 { a : nat -> nat -> R } (ha : forall j, is_lim_seq (fun n => a n j) 0):
+  forall N, is_lim_seq (fun n => sum_f_R0 (fun j => a n j) N) 0.
+Proof.
+  intros N.
+Admitted.
+
+Lemma ash_6_1_1_a {x : nat -> R}{a : nat -> nat -> R} (ha : forall j, is_lim_seq (fun n => (a n j)) 0)
       (hb : forall n, ex_series(fun j => Rabs(a n j)))
       (hx1 : bounded x) (hx2 : is_lim_seq x 0) :
   let y := fun n => Series (fun j => (a n j)*(x j)) in is_lim_seq y 0.
 Proof.
   intros y. destruct hx1 as [M HMx].
-  assert (hy1 : forall n j, Rabs (a n j * x j) <= M*Rabs(a n j)) by admit.
+  assert (hy1 : forall n j, Rabs (a n j * x j) <= M*Rabs(a n j))
+    by (intros ; rewrite Rabs_mult, Rmult_comm ;
+        apply Rmult_le_compat_r; auto; apply Rabs_pos).
   assert (hy2 : forall n M, ex_series(fun j => M*Rabs(a n j)))
     by (intros; now apply (ex_series_scal_l) with (a0 := fun j => Rabs(a n j))).
   assert (hy3 : forall n, ex_series (fun j : nat => Rabs(a n j * x j))).
@@ -57,11 +62,40 @@ Proof.
   rewrite is_lim_seq_Reals; unfold Un_cv, R_dist; simpl.
   intros eps Heps.
   setoid_rewrite Rminus_0_r.
+  assert (hy7 : forall n, exists N, forall j, (j >= N)%nat -> Rabs(x j) < eps/(1 + Series (fun p => Rabs (a n p)))).
+  { intro n.
+    rewrite is_lim_seq_Reals in hx2.
+    assert (Heps' : eps / (1 + Series (fun p => Rabs (a n p))) > 0).
+    {
+      apply Rlt_gt. apply RIneq.Rdiv_lt_0_compat; trivial.
+      apply Rplus_lt_le_0_compat; try lra.
+      replace 0 with (Series (fun _ => 0)).
+      + apply Series_le; trivial; intros.
+        split; try lra; apply Rabs_pos.
+      + apply is_series_unique. rewrite is_series_Reals.
+        rewrite infinite_sum_infinite_sum'. apply infinite_sum'0.
+    }
+    specialize (hx2 (eps/(1 + Series( fun p => Rabs( a n p )))) Heps').
+    unfold R_dist in hx2. setoid_rewrite Rminus_0_r in hx2.
+    exact hx2.
+  }
+  assert (hy8 : forall N, is_lim_seq (fun n => sum_f_R0 (fun j => Rabs (a n j * x j)) N) 0).
+  + intros N.
+    apply (is_lim_seq_le_le (fun _ => 0) _ (fun n => M*sum_f_R0 (fun j => Rabs(a n j)) N)).
+    - intros n.
+      split ; try (apply sum_f_R0_Rabs_pos).
+      rewrite <-sum_f_R0_mult_const.
+      apply sum_f_R0_le; intros.
+      rewrite Rmult_comm.
+      rewrite Rabs_mult.
+      apply Rmult_le_compat_r; auto; try (apply Rabs_pos).
+    - apply is_lim_seq_const.
+    - replace (0:Rbar) with (Rbar_mult M 0) by (apply Rbar_mult_0_r).
+      apply is_lim_seq_scal_l.
+      apply is_lim_seq_sum_f_R0; intros; auto.
+      replace 0 with (Rabs 0) by (apply Rabs_R0).
+      apply is_lim_seq_continuous; auto.
+      rewrite continuity_pt_filterlim; apply (continuous_Rabs 0).
+      + admit.
 
 Admitted.
-
-(*  assert (h : forall n, exists N:nat,
-               Rabs(y n) <=
-               sum_f_R0 (fun j => Rabs ( a n j )* Rabs(x j)) N +
-               Series ((fun j => Rabs (a n (j+N+1)%nat )* Rabs( x (j + N + 1)%nat )))).
-*)
