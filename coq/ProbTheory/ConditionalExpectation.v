605c243ec6539cab5c708705a807eb46b670d337
Avi Shinnar <shinnar@us.ibm.com>
2021-08-23 15:09:07 -0400

    reduce complete to closed

--- coq/ProbTheory/ConditionalExpectation.v
+++ coq/ProbTheory/ConditionalExpectation.v
@@ -398,26 +398,9 @@ Proof.
     now apply NNPP in HH.
   }
   clear HH.
-  assert (forall eps : posreal,
-     F
-       (@Hierarchy.ball
-          (NormedModule.UniformSpace R_AbsRing
-             (@PreHilbert_NormedModule (@L2RRVq_PreHilbert Ts dom prts)))
-          (@LpRRVq_lim Ts dom prts (IZR (Zpos (xO xH))) big2 F) 
-          (pos eps))).
-  {
-    intros.
-    assert (cF':@cauchy (@LpRRVq_UniformSpace Ts dom prts (IZR (Zpos (xO xH))) big2) F).
-    {
-      now apply cauchy_pre_uniform.
-    } 
-    generalize (LpRRVq_lim_complete prts big2 F PF); intros.
-    eapply filter_imp; try eapply (H cF' eps).
-    + intros.
-      unfold Hierarchy.ball; simpl.
-      now apply L2RRVq_ball_ball.
-  }
-  split.
+  match goal with
+    [|- _ /\ ?x ] => cut x; [split; trivial |]
+  end.
   - apply ortho_phi_closed; trivial.
     simpl.
     unfold locally.
@@ -425,19 +408,8 @@ Proof.
     specialize (H eps).
     destruct (HHclassic _ H) as [? [? ?]].
     specialize (HH x).
-    Admitted.
-(*
-    
-  assert (forall eps : posreal, F (Hierarchy.ball (LpRRVq_lim prts big2 F) eps)).
-  split.
-  - apply ortho_phi_closed; trivial.
-    Search LpRRVq_lim.
-    simpl.
-    unfold locally.
-    intros [eps HH].
-    eapply HH.
-    admit.
-    admit.
+    elim HH; trivial.
+    now rewrite <- hball_pre_uniform_eq.
   - intros.
     assert (cF':@cauchy (@LpRRVq_UniformSpace Ts dom prts (IZR (Zpos (xO xH))) big2) F).
     {
@@ -448,138 +420,7 @@ Proof.
     + intros.
       unfold Hierarchy.ball; simpl.
       now apply L2RRVq_ball_ball.
-*)      
-      
-(*
-    unfold Hierarchy.ball; simpl.
-    unfold ball.
-
-
-  
-  generalize (L2RRV_lim_complete (prob_space_sa_sub dom2 sub) big2); intros HH.
-  simpl in *.
-  
-  specialize  (HH (fun s:(LpRRV (prob_space_sa_sub dom2 sub) 2 -> Prop)=>
-                 (LpRRVq_filter_to_LpRRV_filter prts F) (prob_space_sa_sub_set_lift dom2 sub s)
-              )).
-  cut_to HH.
-  - simpl in HH.
-    split.
-    + admit.
-    + intros.
-      specialize (HH eps).
-      assert (eqq:Quot _ (prob_space_sa_sub_LpRRV_lift _ sub ((LpRRV_lim (prob_space_sa_sub dom2 sub) big2
-                  (fun s : LpRRV (prob_space_sa_sub dom2 sub) 2 -> Prop =>
-                     LpRRVq_filter_to_LpRRV_filter prts F (prob_space_sa_sub_set_lift dom2 sub s))))) =
-              (LpRRVq_lim prts big2 F)).
-      {
-        admit.
-      }
-      rewrite <- eqq.
-      eapply filter_imp; try eapply HH; intros a HHa.
-      clear HH.
-      destruct (Quot_inv a); subst.
-      unfold Hierarchy.ball, UniformSpace.ball in *.
-      simpl in *.
-      unfold ball in *.
-      rewrite L2RRVq_norm_norm .
-      unfold minus, plus, opp; simpl.
-      autorewrite with quot.
-      rewrite LpRRVq_normE.
-      
-
-      
-
-      
-      unfold LpRRV_toLpRRVq_set in HHa.
-      specialize (HHa _ (eq_refl _)).
-      
-
-      
-      destruct x; simpl in *.
-      match_destr_in HHa; try tauto.
-      
-
-      * 
-      unfold prob_space_sa_sub_set_lift in HHa.
-      
-      
-      (prob_space_sa_sub_set_lift dom2 sub
-                                  (LpRRVball (prob_space_sa_sub dom2 sub) big2
-                                             q eps)) =
-      (prob_space_sa_sub_LpRRV_lift dom2 sub
-                                             
-                                             
-
-
-
-      
-      unfold LpRRV_toLpRRVq_set in HHa.
-      
-      unfold LpRRVball in HHa.
-      simpl in HHa.
-
-      unfold LpRRVq_filter_to_LpRRV_filter in HH.
-
-
-
-      unfold ball, Hnorm, inner, minus, plus, opp; simpl.
-      
-      
-      
-      unfold LpRRV_toLpRRVq_set in HH.
-
-      
-      unfold LpRRVq_filter_to_LpRRV_filter in HH.
-      
-      unfold LpRRVq_lim.
-      repeat match_destr.
-      * unfold LpRRVq_lim_with_conditions2.
-        rewrite LpRRVq_lim_with_conditionsE.
-        unfold Hierarchy.ball in *; simpl in *.
-        rewrite LpRRVq_ballE.
-        unfold ball, Hnorm.
-
-      
-      assert (rv_eq (LpRRV_lim (prob_space_sa_sub dom2 sub) big2
-                  (fun s : LpRRV (prob_space_sa_sub dom2 sub) 2 -> Prop =>
-                   LpRRVq_filter_to_LpRRV_filter prts F (prob_space_sa_sub_set_lift dom2 sub s)))
-      
-    unfold LpRRVq_filter_to_LpRRV_filter, prob_space_sa_sub_set_lift in HH.
-    unfold LpRRV_toLpRRVq_set in HH.
-    simpl in HH.
-
-
-    
-(*
-
-      generalize (L2RRV_lim_complete (LpRRVq_filter_to_LpRRV_filter F)); intros.
-    generalize (LpRRVq_filter_to_LpRRV_filter_proper F H); intros.
-    generalize (LpRRVq_filter_to_LpRRV_filter_cauchy F H H0); intros.
-
-  
-  
-
-  
-  
-
-  
-  pose (FF := (fun s:(LpRRVq prts 2 -> Prop) => F s /\ forall x, s x -> ortho_phi dom2 x)).
-  
-  generalize (L2RRV_lim_complete prts big2); intros HH2.
-
-  split.
-  - admit.
-  - specialize (HH2 F PF cF).
-  
-
-  
-  
-  unfold ortho_phi in *.
-*)
-*)
-  Admitted.
-
+Qed.
 
 Definition conditional_expectation_L2q
            (dom2 : SigmaAlgebra Ts)
