9ab6b6e8d366726f5e0f7e6a2976cf0874cd4c38
Barry Trager <bmt@us.ibm.com>
2021-09-29 15:12:26 -0400

    IsFiniteExpectation_prod_parts

--- coq/ProbTheory/ConditionalExpectation.v
+++ coq/ProbTheory/ConditionalExpectation.v
@@ -2654,14 +2654,49 @@ Section is_cond_exp.
   Admitted.
 
 
-Lemma IsFiniteExpectation_prod_parts f g :
+Lemma IsFiniteExpectation_prod_parts f g 
+      {rvf : RandomVariable dom borel_sa (rvmult f g)}
+      {rvg : RandomVariable dom2 borel_sa g} :
   IsFiniteExpectation prts (rvmult f g) ->
   IsFiniteExpectation prts (rvmult f (pos_fun_part g)) /\
   IsFiniteExpectation prts (rvmult f (neg_fun_part g)).
 Proof.
   intros.
-  generalize (IsFiniteExpectation_parts prts g); intros.
-  Admitted.
+  split.
+  - pose (P := event_ge dom2 g 0).
+    assert (rv_eq (rvmult f (pos_fun_part g)) (rvmult (rvmult f g) (EventIndicator (classic_dec P)))).
+    {
+      intro x.
+      rv_unfold.
+      rewrite Rmult_assoc.
+      f_equal.
+      simpl.
+      unfold Rmax.
+      repeat match_destr; lra.
+    }
+    generalize (IsFiniteExpectation_indicator prts (rvmult f g) (classic_dec P)); intros.
+    rewrite H0.
+    apply H1; trivial.
+    destruct P.
+    now apply sub.
+  - pose (P := event_ge dom2 (rvopp g) 0).
+    assert (rv_eq (rvmult f (neg_fun_part g)) (rvmult (rvopp (rvmult f g)) (EventIndicator (classic_dec P)))).
+    {
+      intro x.
+      rv_unfold.
+      simpl.
+      unfold Rmax; simpl.
+      repeat match_destr; try lra.
+      assert (g x = 0) by lra.
+      rewrite H0; lra.
+    }
+    generalize (IsFiniteExpectation_opp prts (rvmult f g)); intros.
+    generalize (IsFiniteExpectation_indicator prts (rvopp (rvmult f g)) (classic_dec P)); intros.
+    rewrite H0.
+    apply H2; trivial.
+    destruct P.
+    now apply sub.
+  Qed.
 
   Theorem is_conditional_expectation_factor_out
         f g ce
