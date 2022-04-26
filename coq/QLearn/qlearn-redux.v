5afe7cef5227aaf8c8fcf6849dbe09608e813fe5
Koundinya Vajjha <koundinya.vajjha@gmail.com>
2022-04-26 11:32:28 -0400

    Add offline and online MDP definitions.

--- coq/QLearn/qlearn-redux.v
+++ coq/QLearn/qlearn-redux.v
@@ -17,26 +17,39 @@ Section MDP.
   Arguments outcomes {_}.
   Arguments t {_}.
 
-  (*Record MDP : Type := mkMDP
-  { state : Type;
-    act : state -> Type;
-    st_eqdec : EquivDec.EqDec state eq;
-    fs : Finite state;
-    fa : forall s : state, Finite (act s);
-    ne : mdp.NonEmpty state;
-    na : forall s : state, mdp.NonEmpty (act s);
-    t : forall s : state, act s -> pmf_monad.Pmf state;
-    reward : forall s : state, act s -> state -> R }*)
+  (*TODO(Kody): Change the names of fields. *)
+  Class MDPonline : Type := mkMDP
+  { on_state : Type;
+    on_act : on_state -> Type;
+    on_st_eqdec : EquivDec.EqDec on_state eq;
+    on_fs : Finite on_state;
+    on_fa : forall s : on_state, Finite (on_act s);
+    on_ne : mdp.NonEmpty on_state;
+    on_na : forall s : on_state, mdp.NonEmpty (on_act s);
+    on_t : forall s : on_state, on_act s -> ProbSpace (discrete_sa on_state);
+    on_reward : forall s : on_state, on_act s -> on_state -> R;
+  }.
+
+  Class MDPoffline : Type := mkMDPoffline
+  { off_state : Type;
+    off_act : off_state -> Type;
+    off_st_eqdec : EquivDec.EqDec off_state eq;
+    off_fs : Finite off_state;
+    off_fa : forall s : off_state, Finite (off_act s);
+    off_ne : mdp.NonEmpty off_state;
+    off_na : forall s : off_state, mdp.NonEmpty (off_act s);
+    off_t : (forall s : off_state, off_act s -> off_state) -> ProbSpace (discrete_sa (forall s, off_act s));
+    off_reward : forall s : off_state, off_act s -> off_state -> R;
+  }.
+
 
   (*
     How do we port this over to the new version?
-    1. We will need a new definition of MDPs accounting for the RandomVariable
-       and ProbSpace structure.
-    2.
+    We will need a new definition of MDPs accounting for the RandomVariable
+    and ProbSpace structure.
   *)
 
   Definition bellmanQ' : Rfct(sigT M.(act)) -> M.(state) -> Rfct(sigT M.(act))
   := fun W => fun s' sa => let (s,a) := sa in
                   reward s a s' + γ*Max_{act_list s'}(fun a => W (existT _ s' a)).
 
-  (* How do we port this over to the new version?*)
