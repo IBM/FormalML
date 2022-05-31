8b4b4ccf53c05bb0e9c37b28c98ad96106ac3d9b
Barry Trager <bmt@us.ibm.com>
2022-05-31 09:27:31 +0200

    upper_bound partial upper_bound_step

--- coq/QLearn/qlearn-redux.v
+++ coq/QLearn/qlearn-redux.v
@@ -8,6 +8,8 @@ Require Import RandomVariableFinite RbarExpectation.
 Require Import Classical.
 Require Import SigmaAlgebras ProbSpace.
 
+Set Bullet Behavior "Strict Subproofs".
+
 Open Scope R_scope.
 
 Section MDP.
@@ -49,7 +51,195 @@ Section MDP.
     and ProbSpace structure.
   *)
 
-  Definition bellmanQ' : Rfct(sigT M.(act)) -> M.(state) -> Rfct(sigT M.(act))
-  := fun W => fun s' sa => let (s,a) := sa in
-                  reward s a s' + γ*Max_{act_list s'}(fun a => W (existT _ s' a)).
+  Definition reward_sa (sa : sigT M.(act)) (s' : M.(state)) : R :=
+    let (s, a) := sa in reward s a s'.
 
+  Definition bellmanQ' : Rfct(sigT M.(act)) -> (sigT M.(act) -> M.(state)) -> Rfct(sigT M.(act))
+  := fun W => fun pi sa => let s' := pi sa in
+                  reward_sa sa s' + γ*Max_{act_list s'}(fun a => W (existT _ s' a)).
+
+  Context (alpha : nat -> sigT M.(act) -> R)
+          (pi : nat -> sigT M.(act) -> M.(state))
+          (max_reward min_reward : R)
+          (Q0 : Rfct(sigT M.(act))).
+
+  Fixpoint Q (n : nat) : Rfct(sigT M.(act)) 
+    := match n with
+       | 0 => Q0
+       | (S n') => fun sa =>
+                     (1 - alpha n' sa) * (Q n' sa) + 
+                     (alpha n' sa) *
+                     (bellmanQ' (Q n') (pi n') sa)
+       end.
+
+  Definition s_act_list : list (sigT M.(act)) :=
+    let (la, Hla) := act_finite M in la.
+
+  Lemma s_act_list_not_nil :
+    nil <> s_act_list.
+  Proof.
+    apply not_nil_exists.
+    unfold s_act_list.
+    generalize (ne M); intros.
+    generalize (na M X); intros.
+    exists (existT _ X X0).
+    destruct (act_finite M).
+    apply finite.
+  Qed.
+
+  (* lemma 5 *)
+  Lemma upper_bound_step :
+    0 < γ < 1 ->
+    (forall sa s', reward_sa sa s' <= max_reward) ->
+    (forall t sa, 0 <= alpha t sa < 1) ->
+    forall t,
+      Max_{s_act_list}(Q (S t)) <= Rmax (max_reward/(1-γ)) 
+                                        (Max_{s_act_list}(Q t)).
+  Proof.
+    intros gc rw ac ?.
+    assert (step1: forall t sa,
+               (1 - alpha t sa)*(Q t sa) <= (1 - alpha t sa)*
+                                            (Max_{s_act_list}(Q t))).
+    {
+      intros.
+      apply Rmult_le_compat_l.
+      - specialize (ac t0 sa); lra.
+      - apply Rmax_spec_map.
+        unfold s_act_list.
+        match_destr.
+    }
+    assert (step2: forall (t:nat) (sa:sigT M.(act)),
+               (alpha t sa)*(reward_sa sa (pi t sa) +
+                             γ*Max_{act_list (pi t sa)}
+                                   (fun a => Q t (existT _ (pi t sa) a))) <=
+               (alpha t sa)*(max_reward + γ*Max_{s_act_list}(Q t))).
+    {
+      intros.
+      apply Rmult_le_compat_l.
+      - specialize (ac t0 sa); lra.
+      - apply Rplus_le_compat; trivial.
+        apply Rmult_le_compat_l; try lra.
+        apply Rmax_list_incl.
+        + unfold not; intros.
+          symmetry in H.
+          apply map_eq_nil in H.
+          generalize (act_list_not_nil (pi t0 sa)); intros.
+          now rewrite H in H0.
+        +  admit.
+    }
+    assert (step12: forall (t:nat) (sa:sigT M.(act)),
+               Q (S t) sa <= (1 - alpha t sa)*
+                             (Max_{s_act_list}(Q t)) +
+                             (alpha t sa)*(max_reward + γ*Max_{s_act_list}(Q t))).
+    {
+      intros.
+      specialize (step1 t0 sa).
+      specialize (step2 t0 sa).
+      simpl.
+      unfold bellmanQ'.
+      lra.
+    }
+    assert (cases: forall (t:nat) (sa:sigT M.(act)),
+               Q (S t) sa <= Rmax (max_reward / (1 - γ)) (Max_{ s_act_list}(Q t))).
+    {
+      intros.
+      rewrite Rmax_comm.
+      eapply Rle_trans.
+      apply step12.
+      unfold Rmax.
+      match_destr.
+      - apply Rle_trans with 
+            (r2 :=
+               ((1 - alpha t0 sa) * (max_reward / (1 - γ)) +
+                (alpha t0 sa) * (max_reward + γ * (max_reward / (1 - γ))))).
+        + apply Rplus_le_compat.
+          * apply Rmult_le_compat_l; trivial.
+            specialize (ac t0 sa); lra.
+          * apply Rmult_le_compat_l.
+            -- specialize (ac t0 sa); lra.
+            -- apply Rplus_le_compat_l.
+               apply Rmult_le_compat_l; trivial.
+               lra.
+        + right.
+          field.
+          lra.
+      - apply Rle_trans with 
+            (r2 :=
+               ((1 - alpha t0 sa) * (Max_{ s_act_list}(Q t0)) + 
+                (alpha t0 sa) * ((1-γ)*(Max_{ s_act_list}(Q t0)) + 
+                                 γ * (Max_{ s_act_list}(Q t0))))). 
+        + apply Rplus_le_compat.
+          * apply Rmult_le_compat_l; try lra.
+            specialize (ac t0 sa); lra.
+          * apply Rmult_le_compat_l.
+            -- specialize (ac t0 sa); lra.
+            -- apply Rplus_le_compat; try lra.
+               apply Rmult_le_reg_l with (r := /  (1 - γ)).
+               ++ apply Rinv_0_lt_compat; lra.
+               ++ rewrite <- Rmult_assoc.
+                  rewrite <- Rinv_l_sym; lra.
+        + right.
+          field.
+    }
+    rewrite Rmax_list_le_iff.
+    - intros.
+      rewrite in_map_iff in H.
+      destruct H as [? [? ?]].
+      rewrite <- H.
+      apply cases.
+    - unfold not; intros.
+      symmetry in H.
+      apply map_eq_nil in H.
+      generalize (s_act_list_not_nil); intros.
+      now rewrite H in H0.
+  Admitted.
+
+  Lemma upper_bound :
+    0 < γ < 1 ->
+    (forall sa s', reward_sa sa s' <= max_reward) ->
+    (forall t sa, 0 <= alpha t sa < 1) ->
+    exists (Cmax : R),
+      forall t sa, Q t sa <= Cmax.
+  Proof.
+    intros gc rw ac.
+    exists (Rmax (Max_{ s_act_list}(Q 0))  (max_reward / (1 - γ))).
+    intros.
+    revert sa.
+    induction t.
+    - intros.
+      rewrite Rmax_Rle.
+      left.
+      apply Rmax_spec_map.
+      unfold s_act_list.
+      apply finite.
+    - intros.
+      assert (Max_{ s_act_list}(Q t) <=  Rmax (Max_{ s_act_list}(Q 0)) (max_reward / (1 - γ))).
+      {
+        rewrite Rmax_list_le_iff.
+        - intros.
+          rewrite in_map_iff in H.
+          destruct H as [? [? ?]].
+          rewrite <- H.
+          apply IHt.
+        - unfold not; intros.
+          symmetry in H.
+          apply map_eq_nil in H.
+          generalize (s_act_list_not_nil); intros.
+          now rewrite H in H0.
+      }
+      assert (Q (S t) sa <= (Max_{ s_act_list}(Q (S t)))).
+      {
+        apply Rmax_spec_map.
+        unfold s_act_list.
+        apply finite.
+      }
+      eapply Rle_trans.
+      apply H0.
+      eapply Rle_trans.
+      apply upper_bound_step; try lra; trivial.
+      apply Rle_Rmax.
+      split; trivial.
+      apply Rmax_r.
+  Qed.
+        
+        
