aa8e83279bcacdb0a629df9f2c72ab192e8f10c2
Barry Trager <bmt@us.ibm.com>
2023-08-04 18:28:56 -0400

    cleanup

--- coq/FHE/encode.v
+++ coq/FHE/encode.v
@@ -2323,20 +2323,17 @@ Section unity.
   Qed.
 
   Lemma char_2_opp_eq :
-    (one T = - (one T)) <-> 2%N \in [char T].
+    one T = - (one T) <-> 2%N \in [char T].
   Proof.
+    unfold mem, in_mem; simpl.
+    rewrite mulr2n.
     split;intros.
     - apply (f_equal (fun (z : T) => 1 + z)) in H.
       rewrite addrN in H.
-      unfold char.
-      unfold mem, in_mem; simpl.
-      rewrite -H.
-      by rewrite mulr2n.
-    - unfold mem, in_mem in H; simpl in H.
-      move=> /eqP in H.
+      by rewrite H.
+    - move=> /eqP in H.
       apply (f_equal (fun (z : T) => z - 1)) in H.
-      rewrite add0r in H.
-      by rewrite mulr2n -addrA addrN addr0 in H.
+      by rewrite add0r -addrA addrN addr0 in H.
   Qed.
 
   End unity.
