07057d8f65ffc0343fdb39f58b1794374ce49870
Barry Trager <bmt@us.ibm.com>
2023-08-09 12:19:06 -0400

    FHE encyption and basic ops

---
+++ coq/FHE/encrypt.v
@@ -0,0 +1,72 @@
+Require Import Lia List.
+From mathcomp Require Import common ssreflect fintype bigop ssrnat matrix ring.
+From mathcomp Require Import ssralg ssrfun.
+From mathcomp Require Import generic_quotient ring_quotient.
+From mathcomp Require Import poly mxpoly polydiv ssrint zmodp eqtype ssrbool.
+Import ssralg.GRing.
+Require Import encode.
+
+Set Bullet Behavior "Strict Subproofs".
+
+Local Open Scope ring_scope.
+
+Section encrypt.
+
+  Variable (q:nat).
+  Hypothesis (qodd : (odd q)).
+  
+  Variable (err_poly secret_poly a_poly : {poly 'Z_q}).
+  (* err_poly is small, a_poly is random over 'Z_q *)
+
+  Definition public_key := (-a_poly * secret_poly + err_poly, a_poly).
+
+  Definition encrypt (m : {poly 'Z_q}) := (m + fst public_key, snd public_key).
+
+  Definition encrypt_z (m : {poly int}) := encrypt (red_poly m q).
+
+  Definition decrypt mpair := (fst mpair) + (snd mpair) * secret_poly.
+
+  Lemma encrypt_decrypt (m : {poly 'Z_q}) :
+    decrypt (encrypt m) = m + err_poly.
+  Proof.
+    unfold decrypt, encrypt, public_key, fst, snd.
+    ring.
+  Qed.
+  
+  Definition add_pair (p1 p2 : ({poly 'Z_q} * {poly 'Z_q})) :=
+    (fst p1 + fst p2, snd p1 + snd p2).
+
+  Definition scale_pair (m : {poly 'Z_q}) (p : ({poly 'Z_q} * {poly 'Z_q})) :=
+    (m * fst p, m * snd p).
+
+  Definition mul_pair (p1 p2 : ({poly 'Z_q} * {poly 'Z_q})) :=
+    (fst p1 * fst p2, (fst p1 * snd p2 + snd p1 * fst p2, snd p1 * snd p2)).
+
+  Lemma CKKS_add (m1 m2 : {poly 'Z_q}) :
+    decrypt (add_pair (encrypt m1) (encrypt m2)) =
+      decrypt (encrypt m1) + decrypt(encrypt m2).
+  Proof.
+    rewrite !encrypt_decrypt.
+    unfold add_pair, decrypt, encrypt, public_key, fst, snd.
+    ring.
+  Qed.
+
+  Lemma CKKS_scale (m1 m2 : {poly 'Z_q}) :
+    decrypt (scale_pair m1 (encrypt m2)) = 
+      m1 * decrypt(encrypt m2).
+  Proof.
+    unfold scale_pair, encrypt, decrypt, public_key, fst, snd.
+    ring.
+  Qed.
+
+  Definition decrypt_mul trip := fst trip + secret_poly * decrypt (snd trip).
+
+  Lemma CKKS_mul (m1 m2 : {poly 'Z_q}) :
+    decrypt_mul (mul_pair (encrypt m1) (encrypt m2)) =
+      decrypt (encrypt m1) * decrypt (encrypt m2).
+  Proof.
+    unfold mul_pair, encrypt, decrypt_mul, decrypt, public_key, fst, snd.
+    ring.
+  Qed.
+    
+End encrypt.
