f4c48d6716572d7a0b40472c3b2f9dd1b4a521d3
Barry Trager <bmt@us.ibm.com>
2023-09-29 10:00:54 -0400

    prime_power_dvd_mul_helper

--- coq/FHE/zp_prim_root.v
+++ coq/FHE/zp_prim_root.v
@@ -1108,6 +1108,62 @@ Proof.
   by rewrite ltnn.
 Qed.
   
+Lemma prime_power_dvd_mul_helper p k a b :
+  prime p ->
+  p ^ k %| a * b ->
+  exists j, (p^j %| a) && (p^(k-j) %| b).
+Proof.
+  intros.
+  induction k.
+  - exists 0.
+    by rewrite subn0 !expn0 !dvd1n.
+  - assert (p^k %| a*b).
+    {
+      rewrite expnS in H0.
+      by apply mul_dvdn_r in H0.
+    }
+    specialize (IHk H1).
+    destruct IHk.
+    move /andP in H2.
+    destruct H2.
+    generalize (divnK H2); intros.
+    generalize (divnK H3); intros.
+    rewrite -H4 -H5 expnS in H0.
+    assert (forall k, 0 < p^k).
+    {
+      admit.
+    }
+    assert (xlek: x <= k) by admit.
+    replace (a %/ p ^ x * p ^ x * (b %/ p ^ (k - x) * p ^ (k - x))) with
+      (a %/ p ^ x * (b %/ p ^ (k - x) * p ^ k)) in H0.
+    + rewrite mulnA in H0.
+      rewrite dvdn_pmul2r // in H0.
+      rewrite (Euclid_dvdM _ _ H) in H0.
+      move /orP in H0.
+      destruct H0.
+      * exists (x.+1).
+        apply /andP.
+        split.
+        -- rewrite -H4 expnS dvdn_pmul2r //.
+        -- replace (k.+1-x.+1) with (k-x).
+           ++ apply H3.
+           ++ clear H1 H2 H3 H4 H5 H0.
+              lia.
+      * exists x.
+        apply /andP.
+        split; trivial.
+        rewrite -H5.
+        replace (k.+1-x) with (1 + (k - x)).
+        -- rewrite expnD expn1 dvdn_pmul2r //.
+        -- clear H1 H2 H3 H4 H5 H0.
+           lia.
+   + clear H1 H2 H3 H4 H5 H0.
+     replace (p^k) with (p^x * p^(k-x)); try lia.
+     rewrite -expnD.
+     f_equal.
+     lia.
+Admitted.
+
 Lemma prime_power_dvd_mul p k a b :
   prime p ->
   p ^ k %| a * b = [exists j:(ordinal (k.-1.+1)), (p^j %| a) && (p^(k-j) %| b)].
