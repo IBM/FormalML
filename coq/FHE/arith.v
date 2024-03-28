1ef33c2f5ef4f39e566649cad0410f62aebfb5c3
Barry Trager <bmt@us.ibm.com>
2024-03-28 07:42:39 -0400

    cleanup

--- coq/FHE/arith.v
+++ coq/FHE/arith.v
@@ -154,8 +154,8 @@ Proof.
   rewrite /nearest_round_int /nearest_round /ran_round.
   rewrite mul0r upi0 oppr0 addr0 addrN.
   rewrite /intmul /=.
-  rewrite ssrnum.Num.Theory.ltr_pdivlMr // mul1r.
-  by rewrite /natmul/= ssrnum.Num.Theory.gtrDl ssrnum.Num.Theory.ltr10.
+  rewrite ssrnum.Num.Theory.ltr_pdivlMr //; try lra.
+  by rewrite mul1r /natmul/= ssrnum.Num.Theory.gtrDl ssrnum.Num.Theory.ltr10.    
 Qed.
 
 Lemma nearest_round_int_add (n1 : int) (c : R) :
